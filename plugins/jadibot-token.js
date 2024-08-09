c58016d0a4e6b3e6155ce4fdb1b934afb0589674
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-09 16:33:01 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/jadibot-token.js
+++ plugins/jadibot-token.js
@@ -1,8 +1,8 @@
 import fs from "fs"
 async function handler(m, {usedPrefix}) {
     const user = m.sender.split("@")[0]
-    if (fs.existsSync("./MiniJadiBot/" + user + "/creds.json")) {
-        let token = Buffer.from(fs.readFileSync("./MiniJadiBot/" + user + "/creds.json"), "utf-8").toString("base64")
+    if (fs.existsSync("./MeguminJadiBot/" + user + "/creds.json")) {
+        let token = Buffer.from(fs.readFileSync("./MeguminJadiBot/" + user + "/creds.json"), "utf-8").toString("base64")
         await conn.reply(m.chat, `El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie.\n\n*Tu token es:*`, m, rcanal)
         await conn.reply(m.chat, token, m, rcanal)
     } else {
