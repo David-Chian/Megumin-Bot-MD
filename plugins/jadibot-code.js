87cdbf1294fffbe404ef08d273d9073ead7b461b
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-09 16:29:12 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/jadibot-code.js
+++ plugins/jadibot-code.js
@@ -22,13 +22,13 @@ return conn.reply(m.chat, `「💭」Solo puedes usar este comando en el bot pri
 }
 async function serbot() {
 let authFolderB = crypto.randomBytes(10).toString('hex').slice(0, 8);
-if (!fs.existsSync("./MiniJadiBot/" + authFolderB)) {
-fs.mkdirSync("./MiniJadiBot/" + authFolderB, { recursive: true });
+if (!fs.existsSync("./MeguminJadiBot/" + authFolderB)) {
+fs.mkdirSync("./MeguminJadiBot/" + authFolderB, { recursive: true });
 }
 if (args[0]) {
-fs.writeFileSync(`MiniJadiBot/creds.json`, Buffer.from(args[0], 'base64').toString('utf-8'))
+fs.writeFileSync(`MeguminJadiBot/creds.json`, Buffer.from(args[0], 'base64').toString('utf-8'))
 }
-const { state, saveState, saveCreds } = await useMultiFileAuthState(`./MiniJadiBot/${authFolderB}`);
+const { state, saveState, saveCreds } = await useMultiFileAuthState(`./MeguminJadiBot/${authFolderB}`);
 const msgRetryCounterMap = (MessageRetryMap) => { };
 const msgRetryCounterCache = new NodeCache();
 const { version } = await fetchLatestBaileysVersion();
