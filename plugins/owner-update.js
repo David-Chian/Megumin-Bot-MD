d1963944015c90b1a29dfb6c80edbab846954cd7
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-12 11:53:36 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -20,7 +20,7 @@ const status = execSync('git status --porcelain')
 
 if (status.length > 0) {
 const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
-if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('YoshiSession/') || line.includes('npm-debug.log')) {
+if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('MeguminSession/') || line.includes('npm-debug.log')) {
 return null
 }
 return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
