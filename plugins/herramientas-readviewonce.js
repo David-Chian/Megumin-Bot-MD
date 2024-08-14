d0033830c9525f14d1fa6ee0a6e10e4c67fe79e3
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-14 15:12:38 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/herramientas-readviewonce.js
+++ plugins/herramientas-readviewonce.js
@@ -1,30 +1,25 @@
-const { downloadContentFromMessage } = await import('@whiskeysockets/baileys');
-const handler = async (_0x4828e0, { conn: _0x9bfe62 }) => {
-    if (!_0x4828e0['quoted']) conn.reply(m.chat, '_*🔥 ᏒᎬᏕᏢᎾᏁᎠᎬ Ꭿ ᏬᏁ ᎷᎬᏁᏕᎯᏠᎬ ᏅᏬᎬ ᎻᎯᎽᎯ ᏕᎨᎠᎾ ᎬᏁᏉᎨᎯᎠᎾ ᎬᏁ ᏉᎨᎬᏯᎾᏁᏨᎬ (ᏉᎬᏒ ᏕᎾᏝᎾ ᏬᏁᎯ ᏉᎬᏃ)*_', m, rcanal)
-    if (_0x4828e0['quoted']['mtype'] !== 'viewOnceMessageV2') conn.reply(m.chat, '_*❤‍🔥 ᎬᏝ ᎷᎬᏁᏕᎯᏠᎬ ᏕᎬᏝᎬᏨᏨᎨᎾᏁᎯᎠᎾ ᏁᎾ ᎬᏕ ᏉᎨᎬᏯᎾᏁᏨᎬ (ᏉᎬᏒ ᏕᎾᏝᎾ ᏬᏁᎯ ᏉᎬᏃ)*_', m, rcanal)
+const {downloadContentFromMessage} = (await import('@whiskeysockets/baileys'));
 
-    const _0x12dc2e = _0x4828e0['quoted']['message'];
-    const _0x189b47 = Object.keys(_0x12dc2e)[0x0];
-    const _0x38def8 = await downloadContentFromMessage(_0x12dc2e[_0x189b47], _0x189b47 === 'imageMessage' ? 'image' : 'video');
+const handler = async (m, {conn}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
 
-    let _0x2d1bfa = Buffer.from([]);
-    for await (const _0x249104 of _0x38def8) {
-        _0x2d1bfa = Buffer.concat([_0x2d1bfa, _0x249104]);
-    }
-
-    if (/video/.test(_0x189b47)) {
-        return _0x9bfe62.sendFile(_0x4828e0['chat'], _0x2d1bfa, 'error.mp4', _0x12dc2e[_0x189b47]['caption'] || '', _0x4828e0);
-    } else {
-        if (/image/.test(_0x189b47)) {
-            return _0x9bfe62.sendFile(_0x4828e0['chat'], _0x2d1bfa, 'error.jpg', _0x12dc2e[_0x189b47]['caption'] || '', _0x4828e0);
-        }
-    }
+  if (!m.quoted) conn.reply(m.chat, '_*🔥 ᏒᎬᏕᏢᎾᏁᎠᎬ Ꭿ ᏬᏁ ᎷᎬᏁᏕᎯᏠᎬ ᏅᏬᎬ ᎻᎯᎽᎯ ᏕᎨᎠᎾ ᎬᏁᏉᎨᎯᎠᎾ ᎬᏁ ᏉᎨᎬᏯᎾᏁᏨᎬ (ᏉᎬᏒ ᏕᎾᏝᎾ ᏬᏁᎯ ᏉᎬᏃ)*_', m, rcanal);
+  if (m.quoted.mtype !== 'viewOnceMessageV2') conn.reply(m.chat, '_*❤‍🔥 ᎬᏝ ᎷᎬᏁᏕᎯᏠᎬ ᏕᎬᏝᎬᏨᏨᎨᎾᏁᎯᎠᎾ ᏁᎾ ᎬᏕ ᏉᎨᎬᏯᎾᏁᏨᎬ (ᏉᎬᏒ ᏕᎾᏝᎾ ᏬᏁᎯ ᏉᎬᏃ)*_', m, rcanal)
+  const msg = m.quoted.message;
+  const type = Object.keys(msg)[0];
+  const media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video');
+  let buffer = Buffer.from([]);
+  for await (const chunk of media) {
+    buffer = Buffer.concat([buffer, chunk]);
+  }
+  if (/video/.test(type)) {
+    return conn.sendFile(m.chat, buffer, 'error.mp4', msg[type].caption || '', m);
+  } else if (/image/.test(type)) {
+    return conn.sendFile(m.chat, buffer, 'error.jpg', msg[type].caption || '', m);
+  }
 };
-
-handler['help'] = ['readvo'];
-handler['tags'] = ['tools'];
-handler['command'] = ['readviewonce', 'read', 'revelar', 'readvo'];
-handler['register'] = true;
-handler['group'] = true;
-
+handler.help = ['readvo'];
+handler.tags = ['tools'];
+handler.command = ['readviewonce','read','revelar','readvo'];
 export default handler;
