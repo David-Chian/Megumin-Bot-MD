5d354417bce80e404727948d12a90b7114a51308
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-14 07:57:41 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/info-dashboard.js
+++ plugins/info-dashboard.js
@@ -1,37 +1,38 @@
 let handler = async (m, { conn, command }) => {
 
-if (command == 'dash' || command == 'dashboard' || command == 'views') {
-let stats = Object.entries(db.data.stats).map(([key, val]) => {
-let name = Array.isArray(plugins[key]?.help) ? plugins[key]?.help?.join(' , ') : plugins[key]?.help || key 
+    if (command == 'dash' || command == 'dashboard' || command == 'views') {
+        let stats = Object.entries(db.data.stats).map(([key, val]) => {
+            let name = Array.isArray(plugins[key]?.help) ? plugins[key]?.help?.join(' , ') : plugins[key]?.help || key 
 
-if (/exec/.test(name)) return
-return { name, ...val }
-})
+            if (/exec/.test(name)) return
+            return { name, ...val }
+        })
 
-stats = stats.sort((a, b) => b.total - a.total)
-var handlers = stats.slice(0, 10).map(({ name, total, last }) => {
-return `⬡ *Comando* : *${name}*\n⬡ *Usos* : ${total}`
-}).join`\n\n`
+        stats = stats.sort((a, b) => b.total - a.total)
+        var handlers = stats.slice(0, 10).map(({ name, total }) => {
+            return `⬡ *Comando* : *${name}*\n⬡ *Usos* : ${total}`
+        }).join('\n\n')
 
-conn.reply(m.chat, handlers, m, fake, )
-}
+        conn.reply(m.chat, handlers, m, fake)
+    }
 
-if (command == 'database' || command == 'usuarios' || command == 'user') {
-let totalreg = Object.keys(global.db.data.users).length
-let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    if (command == 'database' || command == 'usuarios' || command == 'user') {
+        let totalreg = Object.keys(global.db.data.users).length
+        let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
 
-conn.reply(m.chat, `${saludo}
+        conn.reply(m.chat, `${saludo} 
 
 🗂️ *Tengo ${rtotalreg} Usuarios Registrados*
 
-📂 *${totalreg} No Estan Registrados*`, m, rcanal)
-}
+📂 *${totalreg} No Están Registrados*`, m, rcanal)
+    }
 
 }
+
 handler.help = ['dash', 'dashboard', 'views', 'database', 'usuarios', 'user']
 handler.tags = ['info']
 handler.command = ['dashboard', 'dash', 'views', 'database', 'usuarios', 'user']
 
 handler.register = true
 
-export default  handler
+export default handler
