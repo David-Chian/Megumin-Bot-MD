624312b9a1685c2661dd4db140b9b041d91499e7
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-15 17:51:25 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/ai-zeta.js
+++ plugins/ai-zeta.js
@@ -6,18 +6,17 @@ const handler = async (m, { conn, command, args, text }) => {
   try {
     let response = await fetch(`https://api.kyuurzy.site/api/ai/aizeta?query=${encodeURIComponent(text)}`);
     let zeta = await response.json();
-    
+
     if (zeta.result && zeta.result.answer) {
-      await conn.sendMessage(m.chat, { text: `${zeta.result.answer}` }, { quoted: m });
+      await conn.sendMessage(m.chat, { text: `${zeta.result.answer}` }, { quoted: m })
     } else {
-      await conn.reply(m.chat, 'No se obtuvo una respuesta válida de la API.', m, rcanal);
+      await conn.reply(m.chat, 'No se obtuvo una respuesta válida de la API.', m, rcanal)
     }
   } catch (e) {
-    console.error(e);
-    await conn.reply(m.chat, 'Hubo un error al procesar tu solicitud.', m, rcanal);
+    await conn.reply(m.chat, `Hubo un error al procesar tu solicitud. ${e}`, m, rcanal);
   }
 }
-handler.tags = ['ai'];
-handler.help = ['zeta <texto>'];
-handler.command = ['zeta'];
+handler.tags = ['ai']
+handler.help = ['zeta <texto>']
+handler.command = ['zeta']
 export default handler
