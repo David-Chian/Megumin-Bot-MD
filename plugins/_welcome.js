2f51fb843e4bc8e40978a3adb9968947ed19500c
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-18 10:28:14 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -5,15 +5,16 @@ export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return true;
 
   let vn = 'https://qu.ax/cTDa.mp3';
-  let welc = welcome
-  let adi = adios
+  let vn2 = 'https://qu.ax/xynz.mp3';
+  let welc = welcome;
+  let adi = adios;
   let chat = global.db.data.chats[m.chat];
-  
   const getMentionedJid = () => {
     return m.messageStubParameters.map(param => `${param}@s.whatsapp.net`);
   };
 
   if (chat.welcome && m.messageStubType === 27) {
+    let userName = await conn.getName(m.messageStubParameters[0]);
     this.sendMessage(m.chat, {
       audio: { url: vn },
       contextInfo: {
@@ -21,7 +22,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
         "externalAdReply": {
           "thumbnail": welc,
           "title": "  ͟͞ Ｗ Ｅ Ｌ Ｃ Ｏ Ｍ Ｅ ͟͞  ",
-          "body": dev,
+          "body": `${userName}!`,
           "previewType": "PHOTO",
           "thumbnailUrl": null,
           "showAdAttribution": true,
@@ -30,36 +31,29 @@ export async function before(m, { conn, participants, groupMetadata }) {
       },
       ptt: true,
       mimetype: 'audio/mpeg',
-      fileName: 'error.mp3'
+      fileName: 'welcome.mp3'
     }, { quoted: fkontak });
   }
 
   if (chat.welcome && (m.messageStubType === 28 || m.messageStubType === 32)) {
+    let userName = await conn.getName(m.messageStubParameters[0]);
     const mentionedJid = getMentionedJid();
-    const text = `Se fue @${m.messageStubParameters[0].split('@')[0]} nadie lo va a extrañar 😹`;
 
     this.sendMessage(m.chat, {
-      text: text,
+      audio: { url: vn2 },
       contextInfo: {
         mentionedJid: mentionedJid,
-        forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363307382381547@newsletter',
-          serverMessageId: '',
-          newsletterName: '⏤͟͞ू⃪ ፝͜⁞M͢ᴇɢ፝֟ᴜᴍ⃨ɪɴ⃜✰⃔࿐/♡ ͟͞ 𓆩ꪶꪾ𝘿᪶𝙞ᷨ𝙖ᷞ𝙢ͣ𝙤᪶ͨ𝙣ᷜ𝙙ꫂৎ୭࠱࠭ ͟͞'
-        },
-        forwardingScore: 9999999,
-        isForwarded: true,
         "externalAdReply": {
           "showAdAttribution": true,
           "containsAutoReply": true,
           "title": '  ͟͞ Ａ Ｄ Ｉ Ｏ́ Ｓ ͟͞  ',
-          body: 'Esperemos que no vuelva -_-',
+          body: `${userName}, se despide.`,
           "previewType": "PHOTO",
           "thumbnailUrl": '',
           "thumbnail": adi,
           "sourceUrl": redes
         }
       }
-    }, { quoted: null, ephemeralExpiration: 24 * 60 * 100, disappearingMessagesInChat: 24 * 60 * 100 });
+    }, { quoted: null });
   }
 }
