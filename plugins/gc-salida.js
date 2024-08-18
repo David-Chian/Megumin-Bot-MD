f0819f9da1ab789199b2070dba7b2d7185c281d0
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-18 13:04:34 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

---
+++ plugins/gc-salida.js
@@ -0,0 +1,18 @@
+setInterval(() => {
+  let userRents = global.db.data.userRents;
+
+  for (let user in userRents) {
+    let rentData = userRents[user];
+
+    rentData.groups.forEach(group => {
+      let groupData = global.db.data.groupRents[group];
+
+      if (groupData && Date.now() >= groupData.startTime + groupData.duration) {
+
+        conn.groupLeave(group);
+        delete global.db.data.groupRents[group];
+        conn.reply(user, `⚠️ Tu token ha expirado y el bot ha salido del grupo ${group}.`, null);
+      }
+    });
+  }
+}, 1000 * 60 * 60); // 1 h
