9126bb9c604f3c9068608421567f0fceab207d71
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-18 16:34:23 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/gc-rentar2.js
+++ plugins/gc-rentar2.js
@@ -1,32 +1,42 @@
-
 import db from '../lib/database.js';
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i;
 
 let handler = async (m, { conn, text, isOwner }) => {
-  if (!text) return m.reply(`> _📝 Ingresa el link del grupo para rentar el bot._`);
+  if (!text) return m.reply(`> _📝 Ingresa el link del grupo para rentar el bot._`, m, rcanal);
 
   let [_, code] = text.match(linkRegex) || [];
-  if (!code) return m.reply('🚩 Enlace inválido.');
+  if (!code) return m.reply('🚩 Enlace inválido.', m, rcanal);
 
   let groupId = await conn.groupAcceptInvite(code);
- 
-  let groupData = global.db.data.groupRents[m.sender]; 
 
-  if (!groupData || Date.now() >= (groupData.startTime + groupData.duration)) {
-    return m.reply('❎ No tienes tokens disponibles para rentar el bot en este grupo. Compra más tokens con /rentar.');
+  global.db.data.groupRents = global.db.data.groupRents || {};
+
+  let userRents = global.db.data.userRents[m.sender];
+  if (!userRents || userRents.tokens <= 0) {
+    return m.reply('❎ No tienes tokens disponibles para rentar el bot. Compra más tokens con /rentar.', m, rcanal);
   }
 
-  conn.reply(m.chat, `> _📝 Me uní correctamente al grupo_ *${groupId}* por ${groupData.tokenCount} día(s).`);
- 
-  let chats = global.db.data.chats[groupId];
-  if (!chats) chats = global.db.data.chats[groupId] = {};
-  
-  if (groupData.duration) chats.expired = groupData.startTime + groupData.duration;
-  
+  global.db.data.groupRents[groupId] = {
+    user: m.sender,
+    tokenCount: userRents.tokens,
+    startTime: Date.now(),
+    duration: userRents.tokens * 24 * 60 * 60 * 1000
+  };
+
+  userRents.tokens = 0;
+
+  userRents.groups.push(groupId);
+
+  conn.reply(m.chat, `> _📝 Me uní correctamente al grupo_ *${groupId}* por ${global.db.data.groupRents[groupId].tokenCount} día(s).`, m, rcanal);
+
+  let chats = global.db.data.chats[groupId] || {};
+  chats.expired = global.db.data.groupRents[groupId].startTime + global.db.data.groupRents[groupId].duration;
+  global.db.data.chats[groupId] = chats;
+
   let pp = 'https://telegra.ph/file/32e696946433c03588726.mp4';
   await conn.sendMessage(groupId, { video: { url: pp }, gifPlayback: true, caption: '> ¡Ya llegué! El bot estará disponible por el tiempo acordado.', mentions: [m.sender] });
 };
-
+handler.tags = ['grupos']
 handler.help = ['rentar2 *<link>*'];
 handler.command = ['rentar2'];
 
