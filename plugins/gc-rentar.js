348380ff49e8cbfa58d6d0aa0230036f40254094
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-18 12:56:51 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/gc-rentar.js
+++ plugins/gc-rentar.js
@@ -1,30 +1,30 @@
 const xpperestrellas = 350;
 const handler = async (m, {conn, command, args}) => {
-  let count = command.replace(/^rentar/i, '');
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperestrellas) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
-  count = Math.max(1, count);
-  
-  if (global.db.data.users[m.sender].exp >= xpperestrellas * count) {
-    global.db.data.users[m.sender].exp -= xpperestrellas * count;
-    global.db.data.users[m.sender].estrellas += count;
+  try {
+    let count = command.replace(/^rentar/i, '');
+    count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperestrellas) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+    count = Math.max(1, count);
 
-    let db = global.db.data.groupRents || {};
-    
-    if (db[m.chat]) {
-      return conn.reply(m.chat, '⚠️ Ya hay una renta activa en este grupo.', m);
-    }
+    if (global.db.data.users[m.sender].exp >= xpperestrellas * count) {
+      global.db.data.users[m.sender].exp -= xpperestrellas * count;
 
-    let groupData = {
-      groupId: m.chat,
-      tokenCount: count,
-      startTime: Date.now(),
-      duration: 24 * 60 * 60 * 1000 * count, // 24 horas por token
-    };
+      let db = global.db.data.groupRents || {};
+      
+      if (db[m.chat]) {
+        return conn.reply(m.chat, '⚠️ Ya hay una renta activa en este grupo.', m);
+      }
 
-    db[m.chat] = groupData;
-    global.db.data.groupRents = db;
+      let groupData = {
+        groupId: m.chat,
+        tokenCount: count,
+        startTime: Date.now(),
+        duration: 24 * 60 * 60 * 1000 * count, // 24 horas 
+      };
 
-    conn.reply(m.chat, `
+      db[m.chat] = groupData;
+      global.db.data.groupRents = db;
+
+      conn.reply(m.chat, `
 ┌─『 𝑅𝑒𝑛𝑡𝑎𝑟 𝑎 𝑀𝑒𝑔𝑢𝑚𝑖𝑛 』*
 │╭──────────────┄
 ││ *Compra Nominal* : + ${count}🌟
@@ -32,10 +32,15 @@ const handler = async (m, {conn, command, args}) => {
 ││ *Utiliza* : .rentar2 + el link
 │╰──────────────┄
 └──────────────`, m);
-  } else {
-    conn.reply(m.chat, `😔 Lo siento, no tienes suficiente *XP* para comprar *${count}* Estrellas 🌟`, m);
+    } else {
+      conn.reply(m.chat, `😔 Lo siento, no tienes suficiente *XP* para comprar *${count}* Estrellas 🌟`, m);
+    }
+  } catch (e) {
+    console.error(e);
+    conn.reply(m.chat, '❌ Ocurrió un error al procesar tu solicitud. Por favor, inténtalo de nuevo.', m);
   }
 };
+
 handler.help = ['rentar'];
 handler.tags = ['grupo'];
 handler.register = true;
