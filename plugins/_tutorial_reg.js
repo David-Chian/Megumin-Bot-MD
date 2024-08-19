0b748950ef6a3048738f8844db8888220d7cbb35
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-19 10:29:18 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/_tutorial_reg.js
+++ plugins/_tutorial_reg.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   const usuario = m.pushName || 'Usuario';
-  const videoPath = './src/tutorial.mp4';
+  const videoUrl = 'https://telegra.ph/file/330a7838421e8f555b7be.mp4';
 
   const texto = `Hola @${m.sender.split('@')[0]} aquí está el tutorial para registrarte en Megumin-Bot.`;
 
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
     mentions: [m.sender]
   };
 
-  await conn.sendMessage(m.chat, { video: { url: videoPath }, ...options });
+  await conn.sendMessage(m.chat, { video: { url: videoUrl }, ...options });
 };
 
 handler.command = ['tutorialreg']
