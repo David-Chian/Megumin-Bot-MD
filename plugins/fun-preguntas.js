f27410a0dfde215ab683e9233c1d091bbecfa079
Xi_miguelon77XX <135562862+CheirZ@users.noreply.github.com>
2024-08-26 10:00:49 -0600

    Actualizar fun-preguntas.js

--- plugins/fun-preguntas.js
+++ plugins/fun-preguntas.js
@@ -1,6 +1,6 @@
 var handler = async (m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🚩 *Ingrese un texto a preguntar*\n\nEjemplo, !${command} Hoy juega Colombia?`, m, rcanal, )
+if (!text) return conn.reply(m.chat, `🚩 *Ingrese un texto a preguntar*\n\nEjemplo, !${command} ¿Hoy estallaremos algo?`, m, rcanal, )
 m.react('❔')
 await delay(1000 * 1)
 m.react('❓')
@@ -8,7 +8,7 @@ await delay(1000 * 1)
 m.react('❔')
 await delay(1000 * 1)
 
-conn.reply(m.chat, `🚩 *Preguntas - LuffyBot*\n\n• *Pregunta:* ${text}\n• *Respuesta:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible','Por que haces estas preguntas','Por eso te dejo','Para que quieres saber','No te dire la respuesta'].getRandom()}`, m, rcanal, )
+conn.reply(m.chat, `🚩 *Preguntas - Megumin-bot💥*\n\n• *Pregunta:* ${text}\n• *Respuesta:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible','Por que haces estas preguntas','Por eso te dejo','Para que quieres saber','No te dire la respuesta'].getRandom()}`, m, rcanal, )
 
 }
 handler.help = ['pregunta']
