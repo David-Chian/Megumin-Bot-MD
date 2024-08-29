bbc65d47521e0e2486a467c5ae635482012294e5
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-08-29 08:09:29 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

---
+++ plugins/_antibots.js
@@ -0,0 +1,22 @@
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
+    if (!m.isGroup) return;
+    let chat = global.db.data.chats[m.chat]
+    let delet = m.key.participant
+    let bang = m.key.id
+    let bot = global.db.data.settings[this.user.jid] || {}
+    if (m.fromMe) return true;
+
+    if (m.id.startsWith('3EB0') && m.id.length === 22) {
+        let chat = global.db.data.chats[m.chat];
+
+        if (chat.antiBot) {
+            await conn.reply(m.chat, "Otros robots detectados\n\nLo siento hermana, tengo que eliminarlo porque el administrador ha activado Anti-Bot :)", null);
+        //    await conn.delay(1000);
+
+            if (isBotAdmin) {
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+            }
+        }
+    }
+}
