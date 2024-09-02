6b05dfe394d48ccef54610fef73bab0afa9c803f
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-09-02 13:05:08 -0400

    Actualizar sunlight.js

--- sunlight.js
+++ sunlight.js
@@ -1,6 +1,5 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './config.js';
-import './plugins/main-allfake.js';
 import { createRequire } from 'module'
 import path, { join } from 'path'
 import {fileURLToPath, pathToFileURL} from 'url'
