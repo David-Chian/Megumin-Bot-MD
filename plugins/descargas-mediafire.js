c46e2c681240b180524f1080ba64e7a743f41235
David Chian <107944279+David-Chian@users.noreply.github.com>
2024-09-03 07:01:54 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/descargas-mediafire.js
+++ plugins/descargas-mediafire.js
@@ -1,51 +1,38 @@
-import axios from 'axios';
-import fetch from 'node-fetch';
-import cheerio from 'cheerio';
-import {mediafiredl} from '@bochilteam/scraper';
-
-const handler = async (m, {conn, args, usedPrefix, command}) => {
-  const datas = global
-
-  if (!args[0]) throw `_*< DESCARGAS - MEDIAFIRE />*_\n\n*[ ℹ️ ] Ingrese un enlace de MediaFire.*\n\n*[ 💡 ] Ejemplo:* _${usedPrefix + command} https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE_`;
-  try {
-    const resEX = await mediafiredl(args[0]);
-    const captionES = `_*DESCARGAS - MEDIAFIRE*_\n
-    ▢ *Nombre:*  ${resEX.filename}
-    ▢ *Tamaño:*  ${resEX.filesizeH}
-    ▢ *Extensión:* ${resEX.ext}\n\n
-    *[ ℹ️ ] Se está enviando el archivo. espere...*`.trim();
-    m.reply(captionES);
-    await conn.sendFile(m.chat, resEX.url, resEX.filename, '', m, null, {mimetype: resEX.ext, asDocument: true});
-  } catch {
-    try {
-      const res = await mediafireDl(args[0]);
-      const {name, size, date, mime, link} = res;
-      const caption = `_*DESCARGAS - MEDIAFIRE*_\n
-      ▢ *Nombre:*  ${name}
-      ▢ *Tamaño:*  ${size}
-      ▢ *Extensión:* ${mime}\n\n
-      *[ ℹ️ ] Se está enviando el archivo. espere...*`.trim();
-      await m.reply(caption);
-      await conn.sendFile(m.chat, link, name, '', m, null, {mimetype: mime, asDocument: true});
-    } catch {
-      await m.reply('Hubo un error en la eescarga.', m, rcanal);
-    }
-  }
-};
-handler.command = ['mediafire','mediafiredl','dlmediafire']
-handler.register = true;
-handler.group = true;
-export default handler;
+import axios from 'axios'
+import fetch from 'node-fetch'
+import cheerio from 'cheerio'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `🧧 Ingrese un enlace de mediafire.\n*Ejemplo:* ${usedPrefix}mediafire https://www.mediafire.com/file/nb63btgjr0dsn4z/COMDELIGHT_v1.1.apk/file`
+try {  
+let res = await mediafireDl(args[0])
+let { name, size, date, mime, link } = res
+let caption = `*Nombre:* ${name}
+*Peso:* ${size}
+*Tipo:* ${mime}`.trim()
+conn.reply(m.chat, caption, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: 'El Bot mas explosivo de WhatsApp', previewType: 0, thumbnail: null, sourceUrl: md}}})
+await conn.sendFile(m.chat, link, name, '', m, null, { mimetype: mime, asDocument: true })
+} catch (e) {
+await conn.reply(m.chat, `Error, pruebe con ${usedPrefix + command}2`)
+console.log(e)
+handler.limit = false      
+}}
+handler.help = ['mediafire'].map(v => v + ' <url>')
+handler.tags = ['descargas']
+handler.command = /^(mediafire|mdfire|mf)$/i
+handler.register = true
+handler.group = true
+export default handler
 
 async function mediafireDl(url) {
-  const res = await axios.get(`https://www-mediafire-com.translate.goog/${url.replace('https://www.mediafire.com/', '')}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`);
-  const $ = cheerio.load(res.data);
-  const link = $('#downloadButton').attr('href');
-  const name = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').attr('title').replaceAll(' ', '').replaceAll('\n', '');
-  const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text();
-  const size = $('#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '').replaceAll(' ', '');
-  let mime = '';
-  const rese = await axios.head(link);
-  mime = rese.headers['content-type'];
-  return {name, size, date, mime, link};
+const res = await axios.get(`https://www-mediafire-com.translate.goog/${url.replace('https://www.mediafire.com/','')}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`)
+const $ = cheerio.load(res.data)
+const link = $('#downloadButton').attr('href')
+const name = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').attr('title').replaceAll(' ','').replaceAll('\n','')
+const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text()
+const size = $('#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '').replaceAll(' ','')
+let mime = ''
+let rese = await axios.head(link)
+mime = rese.headers['content-type']
+return { name, size, date, mime, link }
 }
