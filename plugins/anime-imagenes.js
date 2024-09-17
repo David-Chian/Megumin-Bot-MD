dbe0ec183af1587daced3966d90a29e7a649c94e
おDiego.xyz <151096440+Dev-Diego@users.noreply.github.com>
2024-09-17 11:36:10 -0500

    Actualizar anime-imagenes.js

--- plugins/anime-imagenes.js
+++ plugins/anime-imagenes.js
@@ -1,4 +1,4 @@
-/*import axios from 'axios';
+import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
 const res = (await axios.get(`https://raw.githubusercontent.com/WOTCHITA/YaemoriBot-MD/master/src/JSON/anime-${command}.json`)).data;
 const haha = await res[Math.floor(res.length * Math.random())];
@@ -10,4 +10,4 @@ handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa'
 handler.tags = ['anime'];
 handler.group = true;
 handler.register = true
-export default handler;*/
+export default handler;
