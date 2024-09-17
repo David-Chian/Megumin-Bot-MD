57fa500cd317ecb1b1f763462692d66a38289945
おDiego.xyz <151096440+Dev-Diego@users.noreply.github.com>
2024-09-17 11:32:57 -0500

    prueba waifu megumin

--- plugins/anime-imagenes.js
+++ plugins/anime-imagenes.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+/*import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
 const res = (await axios.get(`https://raw.githubusercontent.com/WOTCHITA/YaemoriBot-MD/master/src/JSON/anime-${command}.json`)).data;
 const haha = await res[Math.floor(res.length * Math.random())];
@@ -8,4 +8,28 @@ handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa'
 handler.tags = ['anime'];
 handler.group = true;
 handler.register = true
-export default handler;
+export default handler;*/
+
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+await m.react('🕓')
+try {
+let res = await fetch('https://api.waifu.pics/sfw/megumin')
+if (!res.ok) return
+let json = await res.json()
+if (!json.url) return
+const messages = [['Imagen 1', dev, await res.json(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.json(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.json(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.json(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, `🚩 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['megumin']
+handler.tags = ['img']
+handler.command = ['megumin']
+//handler.limit = 1
+handler.register = true 
+
+export default handler
