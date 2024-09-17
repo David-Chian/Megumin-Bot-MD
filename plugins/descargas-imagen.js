fcaf91e189494d2ba4166ac470fd5ad31684c895
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-16 21:50:24 -0500

    Actualizar descargas-imagen.js

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -12,7 +12,7 @@ const image = await res.getRandom();
 const link = image;
 const messages = [['Imagen 1', dev, await res.getRandom(),
 [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
-await conn.sendCarousel(m.chat, '🚩 Buscador - Imagenes', `🔍 Resultado de: ${text}`, null, messages, m)     
+await conn.sendCarousel(m.chat, `🚩 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
 };
 handler.help = ['imagen <query>'];
 handler.tags = ['buscador', 'tools', 'descargas'];
