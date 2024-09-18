bb3ee23e5ad160ffffd8862e2d2b67b78448b693
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2024-09-17 21:28:40 -0400

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/fun-casino.js
+++ plugins/fun-casino.js
@@ -21,18 +21,18 @@ return
 cooldowns[m.sender] = Date.now()
 count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
 count = Math.max(1, count)
-if (args.length < 1) return conn.reply(m.chat, '💣 Ingresa la cantidad de ' + `🌟 *Estrellas*` + ' que deseas aportar contra' + ` *Megumin-Bot-MD*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
-if (user.estrellas >= count * 1) {
-user.estrellas -= count * 1
+if (args.length < 1) return conn.reply(m.chat, '💣 Ingresa la cantidad de ' + `🍪 *Cookies*` + ' que deseas aportar contra' + ` *Megumin-Bot-MD*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
+if (user.cookies >= count * 1) {
+user.cookies -= count * 1
 if (Aku > Kamu) {
-conn.reply(m.chat, '`💣 Veamos que numeros tienen!`\n\n'+ `➠ *MeguminBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🌟 Estrellas.`.trim(), m, rcanal)
+conn.reply(m.chat, '`💣 Veamos que numeros tienen!`\n\n'+ `➠ *MeguminBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🍪 Cookies.`.trim(), m, rcanal)
 } else if (Aku < Kamu) {
-user.estrellas += count * 2
-conn.reply(m.chat, '`💥 Veamos que numeros tienen!`\n\n'+ `➠ *MeguminBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🌟 Estrellas.`.trim(), m, rcanal)
+user.cookies += count * 2
+conn.reply(m.chat, '`💥 Veamos que numeros tienen!`\n\n'+ `➠ *MeguminBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🍪 Cookies.`.trim(), m, rcanal)
 } else {
-user.estrellas += count * 1
-conn.reply(m.chat, '`💥 Veamos que numeros tienen!`\n\n'+ `➠ *MeguminBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🌟 Estrellas.`.trim(), m, rcanal)}
-} else conn.reply(m.chat, `No tienes *${formatNumber(count)} 🌟 Estrellas* para apostar!`.trim(), m, rcanal)}
+user.cookies += count * 1
+conn.reply(m.chat, '`💥 Veamos que numeros tienen!`\n\n'+ `➠ *MeguminBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🍪 Cookies.`.trim(), m, rcanal)}
+} else conn.reply(m.chat, `No tienes *${formatNumber(count)} 🍪 Cookies* para apostar!`.trim(), m, rcanal)}
 
 handler.help = ['apostar *<cantidad>*']
 handler.tags = ['fun']
