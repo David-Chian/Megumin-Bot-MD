470b35f5b16c0100748edb3b429447d1ff98567a
おDiego.xyz <151096440+Dev-Diego@users.noreply.github.com>
2024-09-17 22:08:36 -0500

    Actualizar +18-porno.js

--- plugins/+18-porno.js
+++ plugins/+18-porno.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
-if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '🚩 *¡Estos comandos están desactivados!*';
+if (!db.data.chats[m.chat].modohorny && m.isGroup) return m.reply('🚩 *¡Estos comandos están desactivados!*');
 conn.reply(m.chat, `🚩 *Enviando ${command}...*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
 previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
-const res = (await axios.get(`https://raw.githubusercontent.com/WOTCHITA/YaemoriBot-MD/master/src/JSON/${command}.json`)).data;
+const res = (await axios.get(`https://raw.githubusercontent.com/DevDiegoxyz/YaemoriBot-MD/master/src/JSON/${command}.json`)).data;
 const haha = await res[Math.floor(res.length * Math.random())];
 conn.sendFile(m.chat, haha, 'error.jpg', `🔥 *${command}*`, m, null, rcanal);
 };
