a3cd7d41b55b477856069d2cf1f4c5400b0b1dea
おDiego.xyz <151096440+Dev-Diego@users.noreply.github.com>
2024-09-19 10:52:57 -0500

    GAME - FREE FIRE

---
+++ plugins/ff-12vs12.js
@@ -0,0 +1,70 @@
+const handler = async (m, { conn, args }) => {
+    // Verificar si se proporcionaron los argumentos necesarios
+    if (args.length < 2) {
+        conn.reply(m.chat, '🚩 Debes proporcionar la hora (HH:MM) y el color de ropa._', m, rcanal);
+        return;
+    }
+
+    // Validar el formato de la hora
+    const horaRegex = /^([01]\d|2[0-3]):?([0-5]\d)$/;
+    if (!horaRegex.test(args[0])) {
+        conn.reply(m.chat, '🚩 Formato de hora incorrecto. Debe ser HH:MM en formato de 24 horas._', m, rcanal);
+        return;
+    }
+
+    const horaUsuario = args[0]; // Hora proporcionada por el usuario
+    const colorRopa = args.slice(1).join(' '); // Color de ropa proporcionado por el usuario
+
+    // Calcular la hora adelantada
+    const horaUsuarioSplit = horaUsuario.split(':');
+    let horaAdelantada = '';
+    if (horaUsuarioSplit.length === 2) {
+        const horaNumerica = parseInt(horaUsuarioSplit[0], 10);
+        const minutoNumerico = parseInt(horaUsuarioSplit[1], 10);
+        const horaAdelantadaNumerica = horaNumerica + 1; // Adelantar 1 hora
+        horaAdelantada = `${horaAdelantadaNumerica.toString().padStart(2, '0')}:${minutoNumerico.toString().padStart(2, '0')}`;
+    }
+
+    const message = `
+    12 𝐕𝐄𝐑𝐒𝐔𝐒 12
+    
+    𝐇𝐎𝐑𝐀𝐑𝐈𝐎
+    🇲🇽 𝐌𝐄𝐗 : ${horaUsuario}
+    🇨🇴 𝐂𝐎𝐋 : ${horaAdelantada}
+    𝐂𝐎𝐋𝐎𝐑 𝐃𝐄 𝐑𝐎𝐏𝐀: ${colorRopa}
+
+    ¬ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒 𝐏𝐑𝐄𝐒𝐄𝐍𝐓𝐄𝐒
+    
+          𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
+    
+    👑 ┇ 
+    🥷🏻 ┇  
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+          
+         𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 2
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    
+         𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 3
+    
+    👑 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    🥷🏻 ┇ 
+    
+    ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+    🥷🏻 ┇ 
+    🥷🏻 ┇
+    `.trim();
+    
+    conn.sendMessage(m.chat, {text: message}, {quoted: m});
+};
+handler.help = ['12vs12'];
+handler.tags = ['ff'];
+handler.command = ['12vs12', 'vs12'];
+handler.register = true;
+export default handler;
---
+++ plugins/ff-4v4.js
@@ -0,0 +1,84 @@
+const handler = async (m, { conn, args }) => {
+    // Verificar si se proporcionaron los argumentos necesarios
+    if (args.length < 2) {
+         await  conn.reply(m.chat, '🚩 Formato de hora incorrecto, debe ser *HH:MM* en formato de 24 horas.', m, rcanal);
+        return;
+    }
+
+    // Validar el formato de la hora
+    const horaRegex = /^([01]\d|2[0-3]):?([0-5]\d)$/;
+    if (!horaRegex.test(args[0])) {
+      await  conn.reply(m.chat, '🚩 Formato de hora incorrecto, debe ser *HH:MM* en formato de 24 horas.', m, rcanal);
+        return;
+    }
+
+    const horaUsuario = args[0]; // Hora proporcionada por el usuario
+    const pais = args[1].toUpperCase(); // País proporcionado por el usuario
+
+    // Definir la diferencia horaria de cada país con respecto a México
+    const diferenciasHorarias = {
+        MX: 0, // México tiene la misma hora
+        CO: 1, // Colombia tiene una hora más
+        CL: 2, // Chile tiene dos horas más
+        AR: 3  // Argentina tiene tres horas más
+    };
+
+    if (!(pais in diferenciasHorarias)) {
+        conn.reply(m.chat, 'País no válido. Usa MX para México, CO para Colombia, CL para Chile o AR para Argentina.', m);
+        return;
+    }
+
+    // Obtener la diferencia horaria del país seleccionado
+    const diferenciaHoraria = diferenciasHorarias[pais];
+
+    // Calcular las cuatro horas consecutivas en cada país según la hora proporcionada y la diferencia horaria
+    const hora = parseInt(horaUsuario.split(':')[0], 10);
+    const minutos = parseInt(horaUsuario.split(':')[1], 10);
+
+    const horasEnPais = [];
+    for (let i = 0; i < 4; i++) {
+        const horaActual = new Date();
+        horaActual.setHours(hora + i);
+        horaActual.setMinutes(minutos);
+        horaActual.setSeconds(0);
+        horaActual.setMilliseconds(0);
+
+        const horaEnPais = new Date(horaActual.getTime() - (3600000 * diferenciaHoraria)); // Restar la diferencia horaria
+        horasEnPais.push(horaEnPais);
+    }
+
+    // Formatear las horas según el formato de 24 horas y obtener solo la hora y minutos
+    const formatTime = (date) => date.toLocaleTimeString('es', { hour12: false, hour: '2-digit', minute: '2-digit' });
+
+    const horaActual = formatTime(new Date()); // Obtener la hora actual sin modificación
+
+    const message = `
+*4 𝐕𝐄𝐑𝐒𝐔𝐒 4*
+
+🇲🇽 𝐌𝐄𝐗𝐈𝐂𝐎 : ${formatTime(horasEnPais[0])}
+🇨🇴 𝐂𝐎𝐋𝐎𝐌𝐁𝐈𝐀 : ${formatTime(horasEnPais[1])}
+🇨🇱 𝐂𝐇𝐈𝐋𝐄 : ${formatTime(horasEnPais[2])}
+🇦🇷 𝐀𝐑𝐆𝐄𝐍𝐓𝐈𝐍𝐀 : ${formatTime(horasEnPais[3])}
+
+𝐇𝐎𝐑𝐀 𝐀𝐂𝐓𝐔𝐀𝐋 𝐄𝐍 𝐌𝐄𝐗𝐈𝐂𝐎🇲🇽 : ${horaActual}
+
+𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔
+
+👑 ┇ 
+🥷🏻 ┇  
+🥷🏻 ┇ 
+🥷🏻 ┇ 
+
+
+ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+🥷🏻 ┇ 
+🥷🏻 ┇
+`.trim();
+    
+    conn.sendMessage(m.chat, { text: message }, { quoted: m });
+};
+handler.help = ['4vs4'];
+handler.tags = ['ff'];
+handler.command = ['4vs4', 'vs4'];
+handler.register = true;
+export default handler;
