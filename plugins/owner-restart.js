6d821f85ece8485eaf01513eb1234491af864713
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-22 18:53:42 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,18 +1,25 @@
 import { spawn } from 'child_process'
+let handler = async (m, { conn, isROwner, text }) => {
 
-var handler = async (m, { conn, isROwner, text }) => {
+if (!process.send) throw '*『✦』Reiniciar: node sunlight.js*\n*『✦』Reiniciar: node index.js*'
 
-if (!process.send) throw 'Dont: node mini.js\nDo: node index.js'
 if (conn.user.jid == conn.user.jid) {
-await conn.reply(m.chat, '🍟 *R E I N I C I A N D O* 🍟', m, rcanal, )
+
+const { key } = await conn.sendMessage(m.chat, {text: `🗂️ Cargando...`}, {quoted: m})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `📦 Cargando...`, edit: key})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `♻️ Cargando...`, edit: key})
+await conn.sendMessage(m.chat, {text: `*『⛏️』Comenzar reinicio completo...*`, edit: key})
+
 process.send('reset')
 } else throw 'eh'
-
 }
+
 handler.help = ['restart']
 handler.tags = ['owner']
 handler.command = ['restart','reiniciar'] 
-
 handler.rowner = true
 
 export default handler
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
