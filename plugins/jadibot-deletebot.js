e0115306590a5f5826da91a92f28f9f36065c967
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-22 18:46:52 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/jadibot-deletebot.js
+++ plugins/jadibot-deletebot.js
@@ -8,10 +8,10 @@ let uniqid = `${who.split`@`[0]}`
 let userS = `${conn.getName(who)}`
 
 try {
-await fs.rmdir("./MeguminJadiBot/" + uniqid, { recursive: true, force: true })
+await fs.rmdir(`./${jadi}/` + uniqid, { recursive: true, force: true })
 await parentw.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado.' }, { quoted: fkontak })
 } catch(err) {
-if (err.code === 'ENOENT' && err.path === `./MeguminJadiBot/${uniqid}`) {
+if (err.code === 'ENOENT' && err.path === `./${jadi}/${uniqid}`) {
 await parentw.sendMessage(m.chat, { text: "🍟 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
 } else {
 await m.react(error)
