02fc9f7fceb624f1f84fe01141345459cf14c459
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-25 21:45:14 -0500

    © ⍴᥆ᥕᥱrᥱძ ᑲᥡ sᥙᥒᥣіgһ𝗍 𝗍ᥱᥲm ❀

--- plugins/mods-join.js
+++ plugins/mods-join.js
@@ -2,8 +2,8 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 
-if (!text) return m.reply(`💥 Ingresa el enlace del Grupo.`)
 try {
+if (!text) return m.reply(`💥 Ingresa el enlace del Grupo.`)
 let [_, code] = text.match(linkRegex) || []
 if (!code) return m.reply('🐢 Enlace invalido.')
 let res = await conn.groupAcceptInvite(code)
