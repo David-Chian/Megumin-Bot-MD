19983f0455354c146c372296ed17fed91b36f4b8
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-11 11:33:10 -0500

    Actualizar owner-backup.js

--- plugins/owner-backup.js
+++ plugins/owner-backup.js
@@ -7,7 +7,7 @@ await m.react(rwait)
 let d = new Date
 let date = d.toLocaleDateString('es', { day: 'numeric', month: 'long', year: 'numeric' })
 let database = await fs.readFileSync(`./database.json`)
-let creds = await fs.readFileSync(`./${sessions}/creds.json`)
+let creds = await fs.readFileSync(`./MeguminSession/creds.json`)
 await conn.reply(m.chat, `*• Fecha:* ${date}`, m, fake)
 await conn.sendMessage(m.sender, {document: database, mimetype: 'application/json', fileName: `database.json`}, { quoted: fkontak })
 await m.react(done)
