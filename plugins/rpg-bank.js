8316137aff5686b0984f83218b3905d664c99f90
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-20 10:02:29 -0500

    BANK

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -1,18 +1,17 @@
-import db from './lib/database.js'
-let img = 'https://qu.ax/EKcDO.jpg'
+import db from '../lib/database.js'
+let img = icons
 let handler = async (m, {conn, usedPrefix}) => {
    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
    if (who == conn.user.jid) return m.react('✖️')
    if (!(who in global.db.data.users)) return m.reply(`*El usuario no se encuentra en mi base de datos*`)
    let user = global.db.data.users[who]
-   const texto = `${who == m.sender ? `Tienes *${user.bank} Cookies 🍪* en el Banco` : `El usuario @${who.split('@')[0]} tiene *${user.bank} Cookies 🍪* en el Banco`}`
-   
-   await conn.sendButton(m.chat, texto, wm, img, [['Retirar Todo', `${usedPrefix}retirar all`], ['Meter Al Banco Todo', `${usedPrefix}dep all`] ], rcanal, null, { mentions: [who] })
+   const texto = `${who == m.sender ? `🌵 Tienes: *${user.bank}* Galletas en el banco.` : `🌵 Ese usuario tiene *${user.bank}* Galletas 🍪 en el Banco`}`
+
+   await conn.reply(m.chat, texto, m, rcanal)
 }
 
 handler.help = ['bank']
 handler.tags = ['rpg']
 handler.command = ['bank', 'banco'] 
 handler.register = true 
-handler.group = true
 export default handler 
