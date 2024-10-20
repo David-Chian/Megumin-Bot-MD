26c5a6a6c028c2df32a3402dacdba5457fd15cd2
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-20 16:41:19 -0500

    Actualizar fun-69.js

--- plugins/fun-69.js
+++ plugins/fun-69.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let who;
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
     else who = m.chat;
-    if (!db.data.chats[m.chat].nsfwhot && m.isGroup) throw conn.reply(m.chat,  '🚩 *¡Estos comandos están desactivados!*', m, fake);
+    if (!db.data.chats[m.chat].modohorny && m.isGroup) return m.reply('🚩 *¡Estos comandos están desactivados!*');
     if (!who) throw 'Etiqueta o menciona a alguien';
 
     let user = global.db.data.users[who];
