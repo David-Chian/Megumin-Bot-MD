a2e4be85d78ee9357304d85f85ae6a8e4d4184ef
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2024-10-30 13:48:41 -0400

    Actualizar sunlight.js

--- sunlight.js
+++ sunlight.js
@@ -50,24 +50,25 @@ global.timestamp = {start: new Date}
 
 const __dirname = global.__dirname(import.meta.url)
 
-global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-global.prefix = new RegExp('^[' + (opts['prefix'] || '/i!#$%+£¢€¥^°=¶∆×÷π√✓©®&.\\-.').replace(/[|\\{}()[\]^$+*.\-\^]/g, '\\$&') + ']');
+global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+global.prefix = new RegExp('^[/.$#!]')
+// global.opts['db'] = process.env['db']
 
-global.db = new Low(/https?:\/\//.test(opts['db'] || '') ? new cloudDBAdapter(opts['db']) : new JSONFile(`${opts._[0] ? opts._[0] + '_' : ''}database.json`));
+global.db = new Low(/https?:\/\//.test(opts['db'] || '') ? new cloudDBAdapter(opts['db']) : new JSONFile(`src/database/database.json`));
 
-global.DATABASE = global.db; 
+global.DATABASE = global.db 
 global.loadDatabase = async function loadDatabase() {
 if (global.db.READ) {
 return new Promise((resolve) => setInterval(async function() {
 if (!global.db.READ) {
-clearInterval(this);
+clearInterval(this)
 resolve(global.db.data == null ? global.loadDatabase() : global.db.data);
-}}, 1 * 1000));
+}}, 1 * 1000))
 }
-if (global.db.data !== null) return;
-global.db.READ = true;
-await global.db.read().catch(console.error);
-global.db.READ = null;
+if (global.db.data !== null) return
+global.db.READ = true
+await global.db.read().catch(console.error)
+global.db.READ = null
 global.db.data = {
 users: {},
 chats: {},
@@ -76,10 +77,10 @@ msgs: {},
 sticker: {},
 settings: {},
 ...(global.db.data || {}),
-};
-global.db.chain = chain(global.db.data);
-};
-loadDatabase();
+}
+global.db.chain = chain(global.db.data)
+}
+loadDatabase()
 
 const {state, saveState, saveCreds} = await useMultiFileAuthState(global.sessions)
 const msgRetryCounterMap = (MessageRetryMap) => { };
