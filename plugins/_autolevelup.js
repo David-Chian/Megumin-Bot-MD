fab2ef65ebfa0187473f9450a129bcedc8783c1f
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-31 19:54:56 -0500

    Actualizar _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,6 +1,7 @@
-/*import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
 import { levelup } from '../lib/canvas.js'
 export function before(m, { conn }) {
+//if (!db.data.chats[m.chat].autonivel && m.isGroup) throw 
 
 let user = global.db.data.users[m.sender]
 let chat = global.db.data.chats[m.chat]
@@ -9,8 +10,8 @@ return !0
 
 let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
- if (before !== user.level) {
-
-conn.reply(m.chat, `🚩 *SUBISTE DE NIVEL*\n\n🍟 *Nuevo Nivel:* ${user.level}\n✨️ *Nivel Anterior:* ${before}\n🍭 *Rango:* ${user.role}\n🗓 *Fecha:* ${new Date().toLocaleString('id-ID')}`.trim(),m, rcanal, )
+if (before !== user.level) {
+m.reply(`*🎉 ¡ F E L I C I D A D E S ! 🎉*\n\n💫 Nivel Actual » *${user.level}*\n🌵 Rango » *${user.role}*\n📆 Fecha » *${moment.tz('America/Bogota').format('DD/MM/YY')}*\n\n> *\`¡Has alcanzado un Nuevo Nivel!\`*
+`.trim())
     }
-}*/
+} 
