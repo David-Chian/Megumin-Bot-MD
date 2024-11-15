bba01b55df3e05822fa84dbff74a94906a7dcb4d
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-11-14 19:32:18 -0600

    Update rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -1,16 +1,23 @@
+// Bank Editado Por Cuervo
+//★彡[ᴄʀᴇᴀᴛᴇ ʙʏ ᴄᴜᴇʀᴠᴏ-ᴛᴇᴀᴍ-ꜱᴜᴘʀᴇᴍᴇ]彡★
+// Respeten credito xddddd (ratas inmundas)
+import fetch from 'node-fetch'
 import db from '../lib/database.js'
+let img = 'https://qu.ax/qDTDh.png'
 let handler = async (m, {conn, usedPrefix}) => {
    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
    if (who == conn.user.jid) return m.react('✖️')
    if (!(who in global.db.data.users)) return m.reply(`*El usuario no se encuentra en mi base de datos*`)
    let user = global.db.data.users[who]
-   const texto = `${who == m.sender ? `🌵 Tienes: *${user.bank}* Galletas en el banco.` : `🌵 Ese usuario tiene *${user.bank}* Galletas 🍪 en el Banco`}`
-
-  await conn.reply(m.chat, texto, m, rcanal)
+   let name = conn.getName(who);
+   let txt = (`${who == m.sender ? `╭━〔  ${global.packname}  〕⬣\n┋ *Usuario:* ${name}\n┋ *Cookies En Cartera*: ${user.cookies}\n┋ *Cookies En Banco*: ${user.bank}\n┋ *Experiencia:* ${user.exp}\n┋ *Nivel:* ${user.level}\n┋ *Rol:* ${user.role}\n┋ *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣` : `╭━〔  ${global.packname}  〕⬣\n┋ *Usuario:* @${who.split('@')[0]}\n┋ *Cookies En Cartera*: ${user.cookies}\n┋ *Cookies En Banco*: ${user.bank}\n┋ *Experiencia:* ${user.exp}\n┋ *Nivel:* ${user.level}\n┋ *Rol:* ${user.role}\n┋ *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣`}`)
+//await conn.sendButton(m.chat, texto, wm, img, [['Retirar Todo', `${usedPrefix}retirar all`], ['Meter Al Banco Todo', `${usedPrefix}d all`] ], null, { mentions: [who] })
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, {mentions: [who] }, rcanal)
 }
 
 handler.help = ['bank']
 handler.tags = ['rpg']
 handler.command = ['bank', 'banco'] 
 handler.register = true 
+handler.group = true
 export default handler 
