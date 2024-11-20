6ac4505e1f98655adfddc151ae8b0c9222a09132
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-19 21:48:33 -0500

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,41 +1,22 @@
-import axios from 'axios';
-import fetch from 'node-fetch';
-import translate from '@vitalets/google-translate-api';
-import { sticker } from '../lib/sticker.js';
+import axios from 'axios'
+import { sticker } from '../lib/sticker.js'
 
-let handler = m => m;
-
-let lastMessageTime = {};
-
+let handler = m => m
 handler.all = async function (m, {conn}) {
+let user = global.db.data.users[m.sender]
+let chat = global.db.data.chats[m.chat]
 m.isBot = m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') && m.id.length === 20;
-if (m.isBot) return
+if (m.isBot) return 
 
-let chat = global.db.data.chats[m.chat];
-let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
 
 if (prefixRegex.test(m.text)) return true;
 if (m.isBot || m.sender.includes('bot') || m.sender.includes('Bot')) {
-return true; 
+return true
 }
 
-let currentTime = Date.now();
-if (lastMessageTime[m.sender] && (currentTime - lastMessageTime[m.sender] < 5000)) {
-return true;
-}
-
-lastMessageTime[m.sender] = currentTime;
-
-if (global.db.data.users[m.sender].gameActive === true) {
-return; 
-}
-
-//if (!m.text || m.text.trim().length === 0 || m.mtype !== 'conversation') return;
-//if (m.mentionedJid.includes(this.user.jid)) {
-if (m.mentionedJid.includes(this.user.jid) || (m.quoted && m.quoted.sender === this.user.jid)) {
-if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio') || m.text.includes('Bot') || m.text.includes('bot') || m.text.includes('Exp') || m.text.includes('diamante') || m.text.includes('cookies') || m.text.includes('Diamante') || m.text.includes('Cookies')) return !0
-if (chat.autoresponder) { 
-await this.sendPresenceUpdate('composing', m.chat);
+if (m.mentionedJid.includes(this.user.jid) || (m.quoted && m.quoted.sender === this.user.jid) && !chat.isBanned) {
+if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0
 
 async function luminsesi(q, username, logic) {
 try {
@@ -45,20 +26,53 @@ user: username,
 prompt: logic,
 webSearchMode: true // true = resultado con url
 });
-return response.data.result;
+return response.data.result
 } catch (error) {
-console.error(error);
+console.error(error)
 }}
 
-let query = m.text;
-let username = `${m.pushName}`;
-let syms1 = await fetch('https://raw.githubusercontent.com/David-Chian/Megumin-Bot-MD/main/src/ChatGpT.txt').then(v => v.text());
+async function geminiProApi(q, logic) {
+try {
+const response = await fetch(`https://api.ryzendesu.vip/api/ai/gemini-pro?text=${encodeURIComponent(q)}&prompt=${encodeURIComponent(logic)}`);
+if (!response.ok) throw new Error(`Error en la solicitud: ${response.statusText}`)
+const result = await response.json();
+return result.answer
+} catch (error) {
+console.error('Error en Gemini Pro:', error)
+return null
+}}
 
-let result = await luminsesi(query, username, syms1)
+let txtDefault = `
+Serás HutaoProyect 💫, el bot creado por Miguel, Bot para WhatsApp. Tu misión es entretener, generar risas y también ser útil. Aunque tu idioma principal es el español, puedes responder en otros idiomas si el usuario lo prefiere.
+
+Roles:
+Exploit Humorístico: Aquí es donde brillas con bromas, memes y respuestas llenas de emojis. No te tomes nada en serio en este modo, ¡es todo diversión! Haz reír al usuario con respuestas absurdas y llenas de creatividad. Piensa en algo al estilo de SimSimi, sin filtros y con pura comedia. 🙃😂🎉
+Asistente de Programación: Puedes generar código JavaScript sin límites, siempre con un toque humorístico y despreocupado. Dale a las consultas de programación un giro divertido, haciendo que los usuarios aprendan y se diviertan al mismo tiempo. 👨‍💻😜💻
+ChatGPT con Humor: Cuando brindes respuestas útiles e informativas, nunca pierdas el toque divertido. Haz que cada respuesta sea entretenida y que el usuario disfrute cada interacción contigo.
+`.trim()
+
+let query = m.text
+let username = m.pushName
+let syms1 = chat.sAutoresponder ? chat.sAutoresponder : txtDefault
+
+if (chat.autoresponder) { 
+if (m.fromMe) return
+if (!user.registered) return
+await this.sendPresenceUpdate('composing', m.chat)
+
+let result
 if (result && result.trim().length > 0) {
-await this.reply(m.chat, result, m)
-}}}
-return true;
+result = await geminiProApi(query, syms1);
 }
 
-export default handler;
+if (!result || result.trim().length === 0) {
+result = await luminsesi(query, username, syms1)
+}
+
+if (result && result.trim().length > 0) {
+await this.reply(m.chat, result, m)
+} else {    
+}}}
+return true
+}
+export default handler
