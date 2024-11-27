5c3eafa41e5d09f6fe4d0c06444781225f30b718
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-26 19:37:07 -0500

    Actualizar jadibot-token.js

--- plugins/jadibot-token.js
+++ plugins/jadibot-token.js
@@ -3,13 +3,13 @@ import fs from 'fs'
 async function handler(m, {usedPrefix}) {
 
 const user = m.sender.split('@')[0]
-if (fs.existsSync('./MeguminJadiBot/' + user + '/creds.json')) {
-let token = Buffer.from(fs.readFileSync('./MeguminJadiBot/' + user + '/creds.json'), 'utf-8').toString('base64')
+if (fs.existsSync(`./${jadi}/` + user + '/creds.json')) {
+let token = Buffer.from(fs.readFileSync(`./${jadi}/` + user + '/creds.json'), 'utf-8').toString('base64')    
 
-await conn.reply(m.chat, `🎌 *El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie*\n\nTu token es:`, m, fake, )
-await conn.reply(m.chat, token, m, fake, )
+await conn.reply(m.chat, `🍄 *El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie*\n\nTu token es:`, m, rcanal)
+await conn.reply(m.sender, token, m, fake)
 } else {
-await conn.reply(m.chat, `🚩 *No tienes ningun token activo, usa !jadibot para crear uno*`, m, fake, )
+await conn.reply(m.chat, `🚩 *No tienes ningun token activo, usa !jadibot para crear uno*`, m, fake)
 }
 
 }
