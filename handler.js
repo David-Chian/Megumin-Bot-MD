4ea9849564b90c0d2d853d82b35f790bc2011d72
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-27 08:50:45 -0500

    By: DevDiego

--- handler.js
+++ handler.js
@@ -298,9 +298,6 @@ continue
 if (plugin.disabled)
 continue
 const __filename = join(___dirname, name)
-if (m.sender === this.user.jid) {
-continue
-}
 if (typeof plugin.all === 'function') {
 try {
 await plugin.all.call(this, m, {
