a825cd38a3af078c5618570f185b93702fecfc02
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-27 08:58:03 -0500

    By: DevDiego

--- handler.js
+++ handler.js
@@ -274,10 +274,9 @@ if (opts['swonly'] && m.chat !== 'status@broadcast') return
 if (typeof m.text !== 'string')
 m.text = ''
 
-        //if (m.isBaileys) return 
-        if (m.isBaileys || isBaileysFail && m?.sender === this?.this?.user?.jid) {
-        return
-        }
+if (m.isBaileys) {
+return
+}
 m.exp += Math.ceil(Math.random() * 10)
 let usedPrefix
 let _user = global.db.data && global.db.data.users && global.db.data.users[m.sender]
@@ -298,6 +297,9 @@ continue
 if (plugin.disabled)
 continue
 const __filename = join(___dirname, name)
+if (m.sender === this.user.jid) {
+continue
+}
 if (typeof plugin.all === 'function') {
 try {
 await plugin.all.call(this, m, {
