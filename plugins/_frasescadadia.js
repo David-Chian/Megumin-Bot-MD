d9a8da85f3dc16695acb195a89a6107bf2c4d349
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-11-30 18:04:48 -0500

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

---
+++ plugins/_frasescadadia.js
@@ -0,0 +1,18 @@
+// Powered By >> OfcKing
+
+export function before(m, { conn }) {
+  const frases = [
+    "¡Sigue adelante! Cada paso cuenta.",
+    "¡Eres increíble! No te detengas.",
+    "¡Confía en ti mismo! El éxito está a la vuelta de la esquina.",
+    "¡No te rindas! Los grandes logros requieren tiempo.",
+    "¡Tu esfuerzo vale la pena! Sigue así."
+  ]
+
+  const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)]
+
+  // Enviar la frase después de 1 minuto
+  setTimeout(() => {
+    conn.reply(channelid, `🚩 ${fraseAleatoria}`, null, fake)
+  }, 60000)  // 60000 ms = 1 minuto
+}
