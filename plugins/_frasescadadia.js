f78d835c4ee95d56801f1bc3f0183ed25c88ad43
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-11-30 18:11:50 -0500

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/_frasescadadia.js
+++ plugins/_frasescadadia.js
@@ -1,11 +1,8 @@
 // Powered By >> OfcKing
 
 const frases = [
-  "¡Sigue adelante! Cada paso cuenta.",
-  "¡Eres increíble! No te detengas.",
-  "¡Confía en ti mismo! El éxito está a la vuelta de la esquina.",
-  "¡No te rindas! Los grandes logros requieren tiempo.",
-  "¡Tu esfuerzo vale la pena! Sigue así."
+  "Peueba1",
+  "Prueba2"
 ];
 
 function enviarFrase(conn) {
