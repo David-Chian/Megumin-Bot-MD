d847fc8683ca959ba880ec2d0ec409e53a372b78
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-11-30 18:27:42 -0500

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/_frasescadadia.js
+++ plugins/_frasescadadia.js
@@ -1,21 +1,13 @@
 // Powered By >> OfcKing
 
 const frases = [
-  "Hola",
-  "78"
+  "",
+  ""
 ];
 
-let ultimaFraseIndex = -1;
-
 function enviarFrase() {
-  let nuevaFraseIndex;
-  do {
-    nuevaFraseIndex = Math.floor(Math.random() * frases.length);
-  } while (nuevaFraseIndex === ultimaFraseIndex);
-
-  ultimaFraseIndex = nuevaFraseIndex;
-  const fraseAleatoria = frases[nuevaFraseIndex];
-    conn.reply(idchannel, `🚩 ${fraseAleatoria}`, null, fake);
+  const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
+//  conn.reply(idchannel, `${fraseAleatoria}`, null, fake);
 }
 
 // Enviar frase cada minuto (60000 ms)
