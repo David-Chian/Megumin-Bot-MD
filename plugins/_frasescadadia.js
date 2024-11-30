fa3af260c2013c7bd0991efb740b048c87193a92
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-11-30 18:41:03 -0500

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/_frasescadadia.js
+++ plugins/_frasescadadia.js
@@ -1,13 +1,23 @@
 // Powered By >> OfcKing
 
-const frases = [
-  "",
-  ""
-];
+let frases = [];
+
+fs.readFile('./src/FRASE/frases.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error al leer el archivo JSON:', err);
+    return;
+  }
+  const jsonData = JSON.parse(data);
+  frases = jsonData.frasesMotivadoras;
+});
 
 function enviarFrase() {
+  if (frases.length === 0) {
+    conn.reply(idchannel, '👏 No hay frases disponibles, por enviar.', null, fake);
+    return;
+  }
   const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
-//  conn.reply(idchannel, `${fraseAleatoria}`, null, fake);
+  conn.reply(idchannel, `${fraseAleatoria}`, null, fake);
 }
 
 // Enviar frase cada minuto (60000 ms)
