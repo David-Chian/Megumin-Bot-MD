1c1737201f6f4fd6b72a12af1fae5652b6803815
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-11-30 19:57:53 -0500

    🔥⭑𝗠𝗲𝗴𝘂𝗺𝗶𝗻-𝗕𝗼𝘁⭑(^_^♪)

--- plugins/_frasescadadia.js
+++ plugins/_frasescadadia.js
@@ -4,8 +4,9 @@ import fs from "fs";
 
 let frases = [];
 let frasesEnviadas = [];
-let bot = global.db.data.settings[conn.user.jid];
 
+let Frases = global.db.data.settings[this.user.jid].frases  
+if (!frases) return
 fs.readFile('./src/FRASE/frases.json', 'utf8', (err, data) => {
   if (err) {
     console.error('Error al leer el archivo JSON:', err);
@@ -15,21 +16,18 @@ fs.readFile('./src/FRASE/frases.json', 'utf8', (err, data) => {
   frases = jsonData.frasesMotivadoras;
 });
 
-if (bot.frases) { 
 function enviarFrase() {
   if (frases.length === 0) {
     conn.reply(idchannel, '🍄 No hay frases disponibles para enviar', null, fake);
     return;
-  }}
-
-  if (bot.frases) { 
+  }
+ 
   if (frasesEnviadas.length === frases.length) {
     conn.reply(idchannel, '✨️ Todas las frases ya fueron enviadas, reiniciaremos la raíz para que se vuelven a enviar las frases antiguas!', null, fake);
       frasesEnviadas = []; 
     return;
-  }}
-
-  if (bot.frases) { 
+  }
+ 
   let fraseAleatoriaIndex;
   do {
     fraseAleatoriaIndex = Math.floor(Math.random() * frases.length);
@@ -39,7 +37,7 @@ function enviarFrase() {
 
   const fraseAleatoria = frases[fraseAleatoriaIndex];
   conn.reply(idchannel, `${fraseAleatoria}`, null, fake);
-}}
+}
 
 // Enviar frase cada 1 minuto
 setInterval(enviarFrase, 60000);
