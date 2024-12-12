99181a05ddc33becce062adda521b997ca7681d2
edar123 <78393307+edar123@users.noreply.github.com>
2024-12-11 22:17:06 -0600

    Update fun-mamada.js

--- plugins/fun-mamada.js
+++ plugins/fun-mamada.js
@@ -27,7 +27,13 @@ let handler = async (m, { conn, usedPrefix }) => {
     let pp8 = 'https://telegra.ph/file/c799ea8a1ed0fd336579c.mp4'
     let pp9 = 'https://telegra.ph/file/7352d18934971201deed5.mp4' 
     let pp10 = 'https://telegra.ph/file/379edd38bac6de4258843.mp4'
-    const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10];
+    let pp11 = 'https://qu.ax/dYARO.mp4'
+    let pp12 = 'https://qu.ax/pgUHh.mp4'
+    let pp13 = 'https://qu.ax/ZrUkJ.mp4'
+    let pp14 = 'https://qu.ax/yyqRx.mp4'
+    let pp15 = 'https://qu.ax/Yccxx.mp4'
+    let pp16 = 'https://qu.ax/dYARO.mp4'
+    const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15, pp16];
     const video = videos[Math.floor(Math.random() * videos.length)];
     conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption:str, mentions: [m.sender] },{ quoted: estilo })
     };
