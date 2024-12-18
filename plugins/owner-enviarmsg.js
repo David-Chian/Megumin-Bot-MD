ac3a8c7dbfeb787fe281de5df03d675b5171d65b
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-18 12:19:05 -0500

    Actualizar owner-enviarmsg.js

--- plugins/owner-enviarmsg.js
+++ plugins/owner-enviarmsg.js
@@ -1,55 +1,51 @@
 // By: @OfcKing  
 
-import { webp2png } from '../lib/webp2mp4.js';
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
+import * as fs from 'fs';
+import { sticker } from '../lib/sticker.js';
 import uploadFile from '../lib/uploadFile.js';
 import uploadImage from '../lib/uploadImage.js';
-import axios from 'axios';
-import fs from 'fs'; 
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { webp2png } from '../lib/webp2mp4.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const idgroup = global.idchannel
+const idgp = "120363351999685409@g.us"; 
 
-const idgroup = "120363351999685409@g.us";
+var handler = async (m, { conn, text }) => {
+    if (!m.quoted && !text) return conn.reply(m.chat, `🚩 Por favor, escribe tu mensaje o cita el contenido que deseas enviar.`, m);
 
-let handler = async (m, { conn, text }) => {
-    let who = m.mentionedJid && m.mentionedJid.length > 0 ? m.mentionedJid[0] : (m.fromMe ? conn.user.jid : m.sender);
-
-    if (!text && !m.quoted) {
-        return m.reply(`*🚩 Por favor, escribe tu mensaje o cita el contenido que deseas enviar.*`);
-    }
-
-    let content = m.quoted ? m.quoted : m;
-    let messageOptions = {};
-    let mediaBuffer;
-    let mime = (content.msg || content).mimetype || '';
     let messageType = 'un texto'; 
+    let isMedia = false;
+    let quoted, mime, mediax, htextos;
 
     try {
-        if (/image/.test(mime)) {
-            mediaBuffer = await content.download();
-            let imageUrl = await uploadImage(mediaBuffer);
-            messageOptions = { image: { url: imageUrl }, caption: text || content.message?.imageMessage?.caption || '' };
-            messageType = text ? 'una imagen con texto' : 'una imagen';
-        } else if (/video/.test(mime)) {
-            mediaBuffer = await content.download();
-            let videoUrl = await uploadFile(mediaBuffer);
-            messageOptions = { video: { url: videoUrl }, caption: text || content.message?.videoMessage?.caption || '' };
-            messageType = text ? 'un video con texto' : 'un video';
-        } else if (/webp/.test(mime)) {
-            mediaBuffer = await content.download();
-            let stickerBuffer = await webp2png(mediaBuffer);
-            messageOptions = { sticker: stickerBuffer };
+        quoted = m.quoted ? m.quoted : m;
+        mime = (quoted.msg || quoted).mimetype || '';
+        isMedia = /image|video|sticker|audio/.test(mime);
+        htextos = `${text ? text : ""}`;
+
+        if (isMedia && quoted.mtype === 'imageMessage') {
+            mediax = await quoted.download?.();
+            await conn.sendMessage(idgroup, { image: mediax, caption: htextos || null }, { quoted: null });
+            messageType = htextos ? 'una imagen con texto' : 'una imagen';
+        } else if (isMedia && quoted.mtype === 'videoMessage') {
+            mediax = await quoted.download?.();
+            await conn.sendMessage(idgroup, { video: mediax, caption: htextos || null }, { quoted: null });
+            messageType = htextos ? 'un video con texto' : 'un video';
+        } else if (isMedia && quoted.mtype === 'audioMessage') {
+            mediax = await quoted.download?.();
+            await conn.sendMessage(idgroup, { audio: mediax, mimetype: 'audio/mp4', fileName: `megumin.mp3` }, { quoted: null });
+            messageType = 'un audio';
+        } else if (isMedia && quoted.mtype === 'stickerMessage') {
+            mediax = await quoted.download?.();
+            await conn.sendMessage(idgroup, { sticker: mediax }, { quoted: null });
             messageType = 'un sticker';
         } else {
-            messageOptions = { text: text || content.message?.conversation || content.message?.extendedTextMessage?.text || '' };
+            await conn.relayMessage(idgroup, { extendedTextMessage: { text: `${htextos}\n` } }, {});
+            messageType = 'un texto';
         }
 
-        await conn.sendMessage(idchannel, messageOptions);
-
-        let senderInfo = `@${who.split('@')[0]} envió ${messageType} para el canal test!`;
-        await conn.sendMessage(idgroup, { text: senderInfo, mentions: [who] });
+        let senderInfo = `✨️ *HuTao-Proyect* ✨️\n\n👤 Usuario » @${m.sender.split('@')[0]}\n🎋 Tipo » ${messageType}\n🪴 Mensaje » ${text ? text : 'No se incluyó ningun texto.'}`;
+        await conn.sendMessage(idgp, { text: senderInfo, mentions: [m.sender] });
 
     } catch (err) {
         console.error('Error al enviar el mensaje:', err);
@@ -57,6 +53,9 @@ let handler = async (m, { conn, text }) => {
     }
 };
 
-handler.command = ['enviarmensaje', 'enviar', 'mensajegroup', 'solicitud' 'sug'];
+handler.help = ['enviar'];
+handler.tags = ['main'];
+handler.command = ['enviar', 'sug', 'solicitud', 'enviarcanal'];
+handler.register = true;
 
 export default handler;
