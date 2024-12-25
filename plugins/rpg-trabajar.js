7cddda97a60ded02d4f25d040e920906e94e20d1
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-25 14:13:21 -0500

    Actualizar rpg-trabajar.js

--- plugins/rpg-trabajar.js
+++ plugins/rpg-trabajar.js
@@ -1,17 +1,17 @@
 let cooldowns = {}
 
 let handler = async (m, { conn, isPrems }) => {
-let user = global.db.data.users[m.sender]
+let user = globalThis.db.data.users[m.sender]
 let tiempo = 5 * 60
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
 const tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-conn.reply(m.chat, `✧ Debes esperar *${tiempo2}* para trabajar de nuevo en este Grupo.`, m, rcanal)
+conn.reply(m.chat, `✧ Debes esperar *${tiempo2}* para trabajar de nuevo en este Grupo.`, m)
 return
 }
 let rsl = Math.floor(Math.random() * 5000)
 cooldowns[m.sender] = Date.now()
 await conn.reply(m.chat, `✿ ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) ${globalThis.monedas}.`, m, rcanal)
-user.exp += rsl
+user.chocolates += rsl
 }
 
 handler.help = ['w', 'work']
