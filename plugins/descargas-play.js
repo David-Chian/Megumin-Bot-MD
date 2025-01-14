26fc25ff4f30e7d2f2a23527eedfd7c68c62008c
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2025-01-14 13:17:53 -0500

    api delirius

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,70 +1,9 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 import yts from 'yt-search';
-import axios from "axios";
+import axios from 'axios';
 
 const formatAudio = ['mp3', 'm4a', 'webm', 'acc', 'flac', 'opus', 'ogg', 'wav'];
 const formatVideo = ['360', '480', '720', '1080', '1440', '4k'];
-
-const ddownr = {
-  download: async (url, format) => {
-    if (!formatAudio.includes(format) && !formatVideo.includes(format)) {
-      throw new Error('Formato no soportado, verifica la lista de formatos disponibles.');
-    }
-
-    const config = {
-      method: 'GET',
-      url: `https://p.oceansaver.in/ajax/download.php?format=${format}&url=${encodeURIComponent(url)}&api=dfcb6d76f2f6a9894gjkege8a4ab232222`,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/91.0.4472.124 Safari/537.36'
-      }
-    };
-
-    try {
-      const response = await axios.request(config);
-
-      if (response.data && response.data.success) {
-        const { id, title, info } = response.data;
-        const { image } = info;
-        const downloadUrl = await ddownr.cekProgress(id);
-
-        return {
-          id: id,
-          image: image,
-          title: title,
-          downloadUrl: downloadUrl
-        };
-      } else {
-        throw new Error('Fallo al obtener los detalles del video.');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
-    }
-  },
-  cekProgress: async (id) => {
-    const config = {
-      method: 'GET',
-      url: `https://p.oceansaver.in/ajax/progress.php?id=${id}`,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/91.0.4472.124 Safari/537.36'
-      }
-    };
-
-    try {
-      while (true) {
-        const response = await axios.request(config);
-
-        if (response.data && response.data.success && response.data.progress === 1000) {
-          return response.data.download_url;
-        }
-        await new Promise(resolve => setTimeout(resolve, 5000));
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
-    }
-  }
-};
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
@@ -102,19 +41,22 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 
     if (command === 'play') {
       try {
-        const api = await ddownr.download(url, 'mp3');
-        const result = api.downloadUrl;
-        await conn.sendMessage(m.chat, { audio: { url: result }, mimetype: "audio/mpeg" }, { quoted: m });
+        const apiUrl = `https://delirius-apiofc.vercel.app/download/ytmp3?url=${encodeURIComponent(url)}`;
+        const response = await axios.get(apiUrl);
+        const { download_url } = response.data;
+
+        await conn.sendMessage(m.chat, { audio: { url: download_url }, mimetype: "audio/mpeg" }, { quoted: m });
       } catch (error) {
         return m.reply(`🪛 *Error:* ${error.message}`);
       }
     } else if (command === 'play2' || command === 'ytmp4') {
       try {
-        const api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp4?apikey=gifted&url=${videoInfo.url}`);
-        const video = await api.json();
-        const result = video.result.download_url;
+        const apiUrl = `https://delirius-apiofc.vercel.app/download/ytmp4?url=${encodeURIComponent(url)}`;
+        const response = await axios.get(apiUrl);
+        const { download_url } = response.data;
+
         await conn.sendMessage(m.chat, {
-          video: { url: result },
+          video: { url: download_url },
           mimetype: "video/mp4",
         }, { quoted: m });
       } catch (error) {
