beca1110a9122edf0ce2a7fcdfcd76251226d153
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-02-08 01:34:22 -0500

    Actualizar start.js

--- megumin/start.js
+++ megumin/start.js
@@ -182,7 +182,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? [`Megumin-Bot-MD`, 'Edge', '20.0.04'] : methodCodeQR ? [`Megumin-Bot-MD`, 'Edge', '20.0.04'] : ['Megumin-Bot-MD', 'Edge', '110.0.1587.56'], 
+browser: opcion == '1' ? [`${nameqr}`, 'Edge', '20.0.04'] : methodCodeQR ? [`${nameqr}`, 'Edge', '20.0.04'] : ['Ubuntu', 'Edge', '110.0.1587.56'],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
