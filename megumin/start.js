5c37dcb18b26db74b9756db9fe8c455ab2602b67
OfcKing <argumedodie@gmail.com>
2025-02-20 09:38:02 -0500

    fix?

--- megumin/start.js
+++ megumin/start.js
@@ -49,54 +49,11 @@ align: 'center',
 colors: ['white']
 })
 
-say(`Multi Device`, {
-font: 'chrome',
-align: 'center',
-colors: ['red']
-})
-
 say(`Developed By • David-Chian`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
 })
-
-/*const rl = createInterface(process.stdin, process.stdout);
-let p = fork();
-p.on('message', data => {
-switch (data) {
-case 'reset':
-p.process.kill();
-isRunning = false;
-start.apply(this, arguments);
-break;
-case 'uptime':
-p.send(process.uptime());
-break;
-}
-});
-p.on('exit', (_, code) => {
-isRunning = false;
-console.error('🚩 Error:\n', code);
-process.exit();
-if (code === 0) return;
-watchFile(args[0], () => {
-unwatchFile(args[0]);
-start(file);
-});
-});
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-if (!opts['test'])
-if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim());
-});
-}
-process.on('warning', (warning) => {
-if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('🚩 Se excedió el límite de Listeners en:');
-console.warn(warning.stack);
-}
-});*/
 
 protoType()
 serialize()
