e0ef0021ef1ee105cf11fa6f121a85fc3edf6e1b
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-03-11 11:05:18 -0400

    Actualizar start.js

--- megumin/start.js
+++ megumin/start.js
@@ -1,6 +1,6 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './config.js' 
-import { meguminJadiBot } from '../plugins/serbot-serbot.js'
+import { meguminJadiBot } from '../plugins/jadibot-serbot.js'
 import { createRequire } from 'module'
 import path, { join } from 'path'
 import {fileURLToPath, pathToFileURL} from 'url'
