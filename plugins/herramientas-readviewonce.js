6b8ce9346b144e5145a5b50996f75cf0fb97256b
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-03-18 13:14:21 -0400

    Actualizar herramientas-readviewonce.js

--- plugins/herramientas-readviewonce.js
+++ plugins/herramientas-readviewonce.js
@@ -1,29 +1,16 @@
-const {downloadContentFromMessage} = (await import('@whiskeysockets/baileys'));
+let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
-const handler = async (m, {conn}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
+let handler = async (m, { conn }) => {
+if (!m.quoted) return conn.reply(m.chat, `${e} Responde a una imagen ViewOnce.`, m)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `${e} Responde a una imagen ViewOnce.`, m)
+let buffer = await m.quoted.download(false);
+if (/videoMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
+} else if (/imageMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
+}}
 
-  if (!m.quoted) { conn.reply(m.chat, '_*🔥 ᏒᎬᏕᏢᎾᏁᎠᎬ Ꭿ ᏬᏁ ᎷᎬᏁᏕᎯᏠᎬ ᏅᏬᎬ ᎻᎯᎽᎯ ᏕᎨᎠᎾ ᎬᏁᏉᎨᎯᎠᎾ ᎬᏁ ᏉᎨᎬᏯᎾᏁᏨᎬ (ᏉᎬᏒ ᏕᎾᏝᎾ ᏬᏁᎯ ᏉᎬᏃ)*_', m, rcanal);
-return;
-}
-  if (m.quoted.mtype !== 'viewOnceMessageV2') { conn.reply(m.chat, '_*❤‍🔥 ᎬᏝ ᎷᎬᏁᏕᎯᏠᎬ ᏕᎬᏝᎬᏨᏨᎨᎾᏁᎯᎠᎾ ᏁᎾ ᎬᏕ ᏉᎨᎬᏯᎾᏁᏨᎬ (ᏉᎬᏒ ᏕᎾᏝᎾ ᏬᏁᎯ ᏉᎬᏃ)*_', m, rcanal)
-return;
-}
-  const msg = m.quoted.message;
-  const type = Object.keys(msg)[0];
-  const media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video');
-  let buffer = Buffer.from([]);
-  for await (const chunk of media) {
-    buffer = Buffer.concat([buffer, chunk]);
-  }
-  if (/video/.test(type)) {
-    return conn.sendFile(m.chat, buffer, 'error.mp4', msg[type].caption || '', m);
-  } else if (/image/.test(type)) {
-    return conn.sendFile(m.chat, buffer, 'error.jpg', msg[type].caption || '', m);
-  }
-};
-handler.help = ['readvo'];
-handler.tags = ['tools'];
-handler.command = ['readviewonce','read','revelar','readvo'];
-export default handler;
+handler.command = ['readviewonce', 'read', 'readvo', 'rvo', 'ver'] 
+handler.group = true;
+
+export default handler
