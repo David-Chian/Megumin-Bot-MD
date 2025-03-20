7ff51c9bb9c2fd4713174aa1c491669c798b0935
OfcKing <argumedodie@gmail.com>
2025-03-20 10:45:41 -0500

    Actualizar gc-delete.js

--- plugins/gc-delete.js
+++ plugins/gc-delete.js
@@ -12,7 +12,6 @@ return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
 handler.help = ['delete']
 handler.tags = ['grupo']
 handler.command = ['del','delete']
-handler.group = false
 handler.admin = true
 handler.botAdmin = true
 
