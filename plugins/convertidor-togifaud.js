08730770d995e025f494a3caa2c54e6ea5bc3d6d
OfcKing <argumedodie@gmail.com>
2025-03-20 10:58:30 -0500

    Actualizar convertidor-togifaud.js

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -1,23 +1,13 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!m.quoted) return conn.reply(m.chat, `🤍 Responde a un *Video.*`, m, rcanal)
-conn.reply(m.chat, global.wait, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
+if (!m.quoted) return conn.reply(m.chat, `🤍 Responde a un *Video.*`, m)
 const q = m.quoted || m
 let mime = (q.msg || q).mimetype || ''
-if (!/(mp4)/.test(mime)) return conn.reply(m.chat, `🤍 Responde a un *Video.*`, m, rcanal)
-await m.react(rwait)
+if (!/(mp4)/.test(mime)) return conn.reply(m.chat, `🤍 Responde a un *Video.*`, m)
 let media = await q.download()
-let listo = '💭 *Aquí Tienes* 💥'
-conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: listo }, { quoted: fkontak })
-await m.react(done)
+conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: '*Listo* 💣' }, { quoted: m })
 }
 handler.help = ['togifaud']
 handler.tags = ['transformador']
-handler.group = true;
 handler.register = true
 handler.command = ['togifaud']
 export default handler
