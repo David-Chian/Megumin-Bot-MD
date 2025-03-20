199c7b523e1dad5e77fd34391240738763195f62
OfcKing <argumedodie@gmail.com>
2025-03-20 10:15:34 -0500

    Actualizar owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, text, command }) => {
+
 let id = text ? text : m.chat  
-let chat = global.db.data.chats[m.chat]
-chat.welcome = false
-await conn.reply(id, `🚩 *Megumin-Bot* Abandona El Grupo, Fué Genial Estar Aquí 👋`) 
+
 await conn.groupLeave(id)
+
 try {  
-chat.welcome = true
+
 } catch (e) {
-await m.reply(`${fg}`) 
-return console.log(e)
+await m.reply(`No puedo salir de este grupo.\n\n> ${e}`) 
 }}
+
 handler.command = ['salir','leavegc','salirdelgrupo','leave']
-handler.group = true
 handler.rowner = true
+
 export default handler
