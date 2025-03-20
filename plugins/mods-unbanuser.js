b59d43692653fb4981c09d4d331e5aeca6ac42ee
OfcKing <argumedodie@gmail.com>
2025-03-20 09:50:41 -0500

    Actualizar mods-unbanuser.js

--- plugins/mods-unbanuser.js
+++ plugins/mods-unbanuser.js
@@ -23,6 +23,6 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
 handler.help = ['unbanuser <@tag>'];
 handler.command = ['unbanuser'];
 handler.tags = ['mods'];
-handler.mods = true;
+handler.rowner = true;
 handler.group = true;
 export default handler;
