154048039676dd4ad6e1d2d99630d3a9264418fc
OfcKing <argumedodie@gmail.com>
2025-03-20 10:45:26 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -2,14 +2,13 @@ var handler = async (m, { conn, args }) => {
 
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '🚩\v' + link, m, rcanal, { detectLink: true })
+conn.reply(m.chat, link, m)
 
 }
 handler.help = ['link']
 handler.tags = ['grupo']
 handler.command = ['link','linkgroup']
 
-handler.group = true
 handler.botAdmin = true
 
 export default handler
