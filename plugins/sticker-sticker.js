eef3507f766e1394ad352932d72c314002efdfb5
OfcKing <argumedodie@gmail.com>
2025-03-20 10:39:39 -0500

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -46,7 +46,6 @@ else return conn.reply(m.chat, '⚠️ *_La conversión ha fallado, intenta envi
 }}
 handler.help = ['stiker <img>', 'sticker <url>']
 handler.tags = ['sticker']
-handler.group = true;
 handler.register = true
 handler.command = ['s', 'sticker', 'stiker']
 
