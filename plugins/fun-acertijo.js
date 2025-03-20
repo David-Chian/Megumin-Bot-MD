f3604914463f24c9484cb37d20847a917ba4a1ec
OfcKing <argumedodie@gmail.com>
2025-03-20 10:52:52 -0500

    Actualizar fun-acertijo.js

--- plugins/fun-acertijo.js
+++ plugins/fun-acertijo.js
@@ -28,7 +28,6 @@ const handler = async (m, {conn, usedPrefix}) => {
 };
 handler.help = ['acertijo'];
 handler.tags = ['fun'];
-handler.group = true;
 handler.register = true
 handler.command = ['acertijo','acert','adivinanza','tekateki']
 export default handler;
