0615126f2818fbc661316ae698ba31729566c307
Alex-X <argumedodie@gmail.com>
2025-03-30 14:31:41 -0500

    Actualizar _infomessage.js

--- plugins/_infomessage.js
+++ plugins/_infomessage.js
@@ -37,7 +37,7 @@ console.log(`${chalk.yellow.bold('[ ⚠️ Archivo Eliminado ]')} ${chalk.greenB
 `${chalk.blue('(Session PreKey)')} ${chalk.redBright('que provoca el "undefined" en el chat')}`
 )}} 
 
-} else if (isBotAdmin && chat.autoRechazar) {
+if (isBotAdmin && chat.autoRechazar) {
 const prefixes = ['6', '90', '963', '966', '967', '249', '212', '92', '93', '94', '7', '49', '2', '91', '48']
 if (prefixes.some(prefix => m.sender.startsWith(prefix))) {
 await conn.groupRequestParticipantsUpdate(m.chat, [m.sender], 'reject')}
