98d0096c7cd542df7b7b1d430f74de7211b5f1e6
Alex-X <argumedodie@gmail.com>
2025-03-30 15:47:35 -0500

    Actualizar _infomessage.js

--- plugins/_infomessage.js
+++ plugins/_infomessage.js
@@ -48,7 +48,7 @@ console.log(`${chalk.yellow.bold('[ ⚠️ Archivo Eliminado ]')} ${chalk.greenB
 `${chalk.blue('(Session PreKey)')} ${chalk.redBright('que provoca el "undefined" en el chat')}`
 )}}
 
-} if (!primaryBot) {
+} if (primaryBot) {
 if (primaryBotConnected && primaryBotInGroup) {
 if (conn.user.jid !== primaryBot) return 
 } if (mainBotInGroup) {
