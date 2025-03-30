086e7987d767d5627ec22d08bdde56608656f9da
Alex-X <argumedodie@gmail.com>
2025-03-30 15:37:01 -0500

    Actualizar _infomessage.js

--- plugins/_infomessage.js
+++ plugins/_infomessage.js
@@ -55,7 +55,7 @@ console.log(`${chalk.yellow.bold('[ ⚠️ Archivo Eliminado ]')} ${chalk.greenB
 `${chalk.blue('(Session PreKey)')} ${chalk.redBright('que provoca el "undefined" en el chat')}`
 )}}
 
-} if for (const user of Object.values(global.db.data.users)) {
+/*} for (const user of Object.values(global.db.data.users)) {
 if (user.premiumTime != 0 && user.premium) {
 if (new Date() * 1 >= user.premiumTime) {
 user.premiumTime = 0;
@@ -63,7 +63,7 @@ user.premium = false;
 const JID = Object.keys(global.db.data.users).find((key) => global.db.data.users[key] === user);
 const usuarioJid = JID.split`@`[0];
 const textoo = `🤍 @${usuarioJid} Se agotó tu tiempo como usuario premium`;
-await this.sendMessage(JID, {text: textoo, mentions: [JID]}, {quoted: ''})}}
+await this.sendMessage(JID, {text: textoo, mentions: [JID]}, {quoted: ''})}}*/
 
 } if (isBotAdmin && chat.antifake) {
 if (m.sender.startsWith('6' || '6')) {
