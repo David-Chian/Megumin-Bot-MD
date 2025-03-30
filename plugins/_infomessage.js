e0e26e810848b80a60751ffe9a7f5254eb90bedf
Alex-X <argumedodie@gmail.com>
2025-03-30 14:57:14 -0500

    Actualizar _infomessage.js

--- plugins/_infomessage.js
+++ plugins/_infomessage.js
@@ -6,7 +6,7 @@ import path from 'path'
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 let handler = m => m
-handler.before = async function (m, { conn, isAdmin, isBotAdmin participants, groupMetadata }) {
+handler.before = async function (m, { conn, isAdmin, isBotAdmin, groupMetadata }) {
 if (!m.messageStubType || !m.isGroup) return
 
 let usuario = `@${m.sender.split`@`[0]}`
