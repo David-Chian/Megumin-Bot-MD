a77f06c9dea66457f5c9ac3575a9a3c0d3479879
Alex-X <argumedodie@gmail.com>
2025-03-30 15:41:09 -0500

    Actualizar _infomessage.js

--- plugins/_infomessage.js
+++ plugins/_infomessage.js
@@ -32,20 +32,23 @@ let who = m.messageStubParameters[0] + '@s.whatsapp.net'
 let user = global.db.data.users[who]
 let userName = user ? user.name : await conn.getName(who)
 
-/*const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])]
+const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])]
 const participants = m.isGroup ? (await conn.groupMetadata(m.chat).catch(() => ({ participants: [] }))).participants : []
 const mainBotInGroup = participants.some(p => p.id === global.conn.user.jid)
 const primaryBot = chat.primaryBot
 const primaryBotConnected = users.some(conn => conn.user.jid === primaryBot)
 const primaryBotInGroup = participants.some(p => p.id === primaryBot)
+
 if (primaryBot) {
-if (primaryBotConnected && primaryBotInGroup) {
+
+else if (primaryBotConnected && primaryBotInGroup) {
 if (this.user.jid !== primaryBot) throw !1
-} else if (mainBotInGroup) {
+
+} if (mainBotInGroup) {
 if (this.user.jid !== global.conn.user.jid) throw !1
 }}
 
-}*/ if (chat.detect && m.messageStubType == 2) {
+} if (chat.detect && m.messageStubType == 2) {
 const uniqid = (m.isGroup ? m.chat : m.sender).split('@')[0]
 const sessionPath = './MeguminSession/'
 for (const file of await fs.readdir(sessionPath)) {
