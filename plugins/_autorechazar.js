6742ff318869508f21f762d2c6eda73f8e891e6a
Alex-X <argumedodie@gmail.com>
2025-03-31 10:15:20 -0500

    Actualizar _autorechazar.js

--- plugins/_autorechazar.js
+++ plugins/_autorechazar.js
@@ -1,3 +1,5 @@
+export async function before(m, { conn, isBotAdmin }) {
+
 if (isBotAdmin && chat.autoRechazar) {
 const prefixes = ['595', '90', '963', '966', '967', '249', '212', '92', '93', '94', '7', '49', '2', '91', '48']
 if (prefixes.some(prefix => m.sender.startsWith(prefix))) {
