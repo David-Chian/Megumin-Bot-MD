8aa8664b7cd03dd3ea8a1b121d5bc4fd4e97f5c1
Alex-X <argumedodie@gmail.com>
2025-03-31 10:20:54 -0500

    Actualizar _infomessage2.js

--- plugins/_infomessage2.js
+++ plugins/_infomessage2.js
@@ -4,7 +4,7 @@ handler.before = async function (m, { conn, isBotAdmin }) {
 let chat = globalThis.db.data.chats[m.chat];
 
 if (isBotAdmin && chat.autoRechazar) {
-const prefixes = ['595', '90', '963', '966', '967', '249', '212', '92', '93', '94', '7', '49', '2', '91', '48']
+const prefixes = ['6', '90', '963', '966', '967', '249', '212', '92', '93', '94', '7', '49', '2', '91', '48']
 if (prefixes.some(prefix => m.sender.startsWith(prefix))) {
 await conn.groupRequestParticipantsUpdate(m.chat, [m.sender], 'reject')}
 
