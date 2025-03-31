a2a8968620a2bc861b11964172c55bf0a4fff62f
Alex-X <argumedodie@gmail.com>
2025-03-31 10:05:10 -0500

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,27 +1,7 @@
-import ws from 'ws';
-
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
 let linkRegex1 = /whatsapp.com\/channel\/([0-9A-Za-z]{20,24})/i;
 
 export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, participants }) {
-
-let chat = globalThis.db.data.chats[m.chat];
-const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
-// const participants = m.isGroup ? (await conn.groupMetadata(m.chat).catch(() => ({ participants: [] }))).participants : []
-
-const mainBotInGroup = participants.some(p => p.id === global.conn.user.jid);
-const primaryBot = chat.primaryBot;
-const primaryBotConnected = users.some(conn => conn.user.jid === primaryBot);
-const primaryBotInGroup = participants.some(p => p.id === primaryBot);
-
-if (m.isGroup) {
-if (primaryBot) {
-if (primaryBotConnected && primaryBotInGroup) {
-if (this.user.jid !== primaryBot) throw !1; 
-}
-else if (mainBotInGroup) {
-if (this.user.jid !== global.conn.user.jid) throw !1;
-}}}
 
 if (!m.isGroup) return;
 if (isAdmin || isOwner || m.fromMe || isROwner) return;
