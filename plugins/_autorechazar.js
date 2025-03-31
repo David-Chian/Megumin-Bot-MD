903d26baae38eec2c8db6d51139bd5a64ce65b77
Alex-X <argumedodie@gmail.com>
2025-03-31 10:14:45 -0500

    Crear _autorechazar.js

---
+++ plugins/_autorechazar.js
@@ -0,0 +1,4 @@
+if (isBotAdmin && chat.autoRechazar) {
+const prefixes = ['595', '90', '963', '966', '967', '249', '212', '92', '93', '94', '7', '49', '2', '91', '48']
+if (prefixes.some(prefix => m.sender.startsWith(prefix))) {
+await conn.groupRequestParticipantsUpdate(m.chat, [m.sender], 'reject')}}
