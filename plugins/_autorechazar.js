61c300b6e2cb9e5f0a1ddf2f07ab9298336b3efc
Alex-X <argumedodie@gmail.com>
2025-03-31 10:16:49 -0500

    Actualizar _autorechazar.js

--- plugins/_autorechazar.js
+++ plugins/_autorechazar.js
@@ -1,6 +1,12 @@
-export async function before(m, { conn, isBotAdmin }) {
+let handler = m => m
+handler.before = async function (m, { conn, isBotAdmin }) {
+
+let chat = globalThis.db.data.chats[m.chat];
 
 if (isBotAdmin && chat.autoRechazar) {
 const prefixes = ['595', '90', '963', '966', '967', '249', '212', '92', '93', '94', '7', '49', '2', '91', '48']
 if (prefixes.some(prefix => m.sender.startsWith(prefix))) {
 await conn.groupRequestParticipantsUpdate(m.chat, [m.sender], 'reject')}}
+
+}
+export default handler
