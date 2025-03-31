0c681f7a5f1cfb056084918c66c664242d2a1d52
Alex-X <argumedodie@gmail.com>
2025-03-31 09:55:33 -0500

    Crear _antilink.js

---
+++ plugins/_antilink.js
@@ -0,0 +1,45 @@
+import ws from 'ws';
+
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+let linkRegex1 = /whatsapp.com\/channel\/([0-9A-Za-z]{20,24})/i;
+
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+
+const participants = m.isGroup ? (await conn.groupMetadata(m.chat).catch(() => ({ participants: [] }))).participants : []
+const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])]
+const mainBotInGroup = participants.some(p => p.id === global.conn.user.jid)
+const primaryBot = chat.primaryBot
+const primaryBotConnected = users.some(conn => conn.user.jid === primaryBot)
+const primaryBotInGroup = participants.some(p => p.id === primaryBot)
+
+if (primaryBot) {
+if (primaryBotConnected && primaryBotInGroup) {
+if (conn.user.jid !== primaryBot) return 
+} if (mainBotInGroup) {
+if (conn.user.jid !== global.conn.user.jid) return
+}}
+
+if (!m.isGroup) return 
+if (isAdmin || isOwner || m.fromMe || isROwner) return
+
+let chat = globalThis.db.data.chats[m.chat];
+let delet = m.key.participant;
+let bang = m.key.id;
+const user = `@${m.sender.split`@`[0]}`;
+const groupAdmins = participants.filter(p => p.admin);
+const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n');
+let bot = globalThis.db.data.settings[this.user.jid] || {};
+const isGroupLink = linkRegex.exec(m.text) || linkRegex1.exec(m.text);
+const grupo = `https://chat.whatsapp.com`;
+if (chat.antilinks && isGroupLink && !isAdmin) {
+if (isBotAdmin) {
+const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
+if (m.text.includes(linkThisGroup)) return !0;
+}
+if (isBotAdmin) {
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+if (responseb[0].status === "404") return;
+}} 
+return !0;
+}
