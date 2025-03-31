0a560d67ed382c354c726454bb1fd8bc271c1441
Alex-X <argumedodie@gmail.com>
2025-03-30 20:46:26 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,75 +1,83 @@
-import { join, dirname } from 'path';
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { setupMaster, fork } from 'cluster';
-import { watchFile, unwatchFile } from 'fs';
-import cfonts from 'cfonts';
-import { createInterface } from 'readline';
-import yargs from 'yargs';
-import chalk from 'chalk';
-console.log('\n✰ Iniciando Megumin ✰');
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const require = createRequire(__dirname);
-const { name, description, author, version } = require(join(__dirname, './package.json'));
-const { say } = cfonts;
-const rl = createInterface(process.stdin, process.stdout);
-say('Megumin-Bot-MD', {
-font: 'block',
-align: 'center',
-colors: ['white']
-});
-say(`Developed By • David-Chian`, {
-font: 'console',
-align: 'center',
-colors: ['yellow']
-});
-var isRunning = false;
+import { join, dirname } from 'path'
+import { createRequire } from 'module'
+import { fileURLToPath } from 'url'
+import { setupMaster, fork } from 'cluster'
+import { watchFile, unwatchFile } from 'fs'
+import cfonts from 'cfonts'
+import { createInterface } from 'readline'
+import yargs from 'yargs'
+import chalk from 'chalk'
+
+console.log(chalk.magentaBright('\n✰ Iniciando Megumin ✰'))
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const require = createRequire(__dirname)
+const { name, description, author, version } = require(join(__dirname, './package.json'))
+const rl = createInterface(process.stdin, process.stdout)
+
+cfonts.say('Megumin-Bot-MD', {
+  font: 'block',
+  align: 'center',
+  colors: ['blue', 'cyan'],
+  gradient: ['magenta', 'yellow']
+})
+
+cfonts.say('Developed By • David-Chian', {
+  font: 'console',
+  align: 'center',
+  colors: ['green', 'yellow']
+})
+
+let isRunning = false
+
 function start(file) {
-if (isRunning) return;
-isRunning = true;
-let args = [join(__dirname, 'megumin', file), ...process.argv.slice(2)];
-say([process.argv[0], ...args].join(' '), {
-font: 'console',
-align: 'center',
-colors: ['green']
-});
-setupMaster({
-exec: args[0],
-args: args.slice(1),
-});
-let p = fork();
-p.on('message', data => {
-switch (data) {
-case 'reset':
-p.process.kill();
-isRunning = false;
-start.apply(this, arguments);
-break;
-case 'uptime':
-p.send(process.uptime());
-break;
+  if (isRunning) return
+  isRunning = true
+
+  let args = [join(__dirname, 'megumin', file), ...process.argv.slice(2)]
+  cfonts.say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    colors: ['white', 'blue']
+  })
+
+  setupMaster({
+    exec: args[0],
+    args: args.slice(1)
+  })
+
+  let p = fork()
+
+  p.on('message', data => {
+    switch (data) {
+      case 'reset':
+        p.process.kill()
+        isRunning = false
+        start(file)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+
+  p.on('exit', (_, code) => {
+    isRunning = false
+    console.error(chalk.redBright('🚩 Error:\n'), code)
+    process.exit()
+    if (code === 0) return
+    watchFile(args[0], () => {
+      unwatchFile(args[0])
+      start(file)
+    })
+  })
 }
-});
-p.on('exit', (_, code) => {
-isRunning = false;
-console.error('🚩 Error:\n', code);
-process.exit();
-if (code === 0) return;
-watchFile(args[0], () => {
-unwatchFile(args[0]);
-start(file);
-});
-});
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-if (!opts['test'])
-if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim());
-});
-}
-process.on('warning', (warning) => {
-if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('🚩 Se excedió el límite de Listeners en:');
-console.warn(warning.stack);
-}
-});
-start('start.js');
+
+process.on('warning', warning => {
+  if (warning.name === 'MaxListenersExceededWarning') {
+    console.warn(chalk.yellow('🚩 Se excedió el límite de Listeners en:'))
+    console.warn(warning.stack)
+  }
+})
+
+start('start.js')
