f696aa4935562fa8c274dfe5c6301c821a8fe053
Alex-X <argumedodie@gmail.com>
2025-03-31 09:37:47 -0500

    Actualizar _infomessage.js

--- plugins/_infomessage.js
+++ plugins/_infomessage.js
@@ -130,16 +130,6 @@ let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 return
 }
 
-} if (Object.values(global.db.data.users)) {
-if (user.premiumTime != 0 && user.premium) {
-if (new Date() * 1 >= user.premiumTime) {
-user.premiumTime = 0;
-user.premium = false;
-const JID = Object.keys(global.db.data.users).find((key) => global.db.data.users[key] === user);
-const usuarioJid = JID.split`@`[0];
-const textoo = `🤍 @${usuarioJid} Se agotó tu tiempo como usuario premium`;
-await conn.sendMessage(JID, {text: textoo, mentions: [JID]}, {quoted: ''})}}
-
 } if (isBotAdmin && chat.antifake) {
 const antiFakePrefixes = ['6', '90', '212', '92', '93', '94', '7', '49', '2', '91', '48']
 if (antiFakePrefixes.some(prefix => m.sender.startsWith(prefix))) {
