8e62ed054a00b9108af89b5b16e7c470e8430d5a
Alex-X <argumedodie@gmail.com>
2025-03-30 22:14:49 -0500

    Actualizar _infomessage.js

--- plugins/_infomessage.js
+++ plugins/_infomessage.js
@@ -19,7 +19,7 @@ const img = imagen1
 const chat = global.db.data.chats[m.chat]
 const mentionsString = [m.sender, m.messageStubParameters[0], ...groupAdmins.map((v) => v.id)]
 const mentionsContentM = [m.sender, m.messageStubParameters[0]]
-const vn = 'https://qu.ax/Deuut.mp3'
+const vn = 'https://qu.ax/OzTbp.mp3'
 const vn2 = 'https://qu.ax/OzTbp.mp3'
 const delet = m.key.participant
 const bang = m.key.id
