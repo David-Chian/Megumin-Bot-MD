70472a59afd4c850f5e2db6ac365d1604c90731e
Alex-X <argumedodie@gmail.com>
2025-03-31 21:39:23 -0500

    Update

--- plugins/gc-infogrupo.js
+++ plugins/gc-infogrupo.js
@@ -1,10 +1,9 @@
 const handler = async (m, {conn, participants, groupMetadata}) => {
   const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => global.icons);
-  const {antiToxic, autoRechazar, autoAceptar, welcome, detect, antiLink, modohorny, primaryBot} = global.db.data.chats[m.chat];
+  const { antiToxic, autoRechazar, autoAceptar, welcome, detect, antiLink, modohorny } = global.db.data.chats[m.chat];
   const groupAdmins = participants.filter((p) => p.admin);
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
-  const primary = primaryBot ? `@${primaryBot.split('@')[0]}` : 'Aleatorio'
   try {
   const text = `💥 *INFO GRUPO*
 💌 *ID:*
@@ -17,8 +16,6 @@ const handler = async (m, {conn, participants, groupMetadata}) => {
 → ${participants.length} Participantes
 👑 *Creador del Grupo:*
 → @${owner.split('@')[0]}
-💣 *Bot primario:*
-→ ${primary}
 🏆 *Administradores:*
 ${listAdmin}
 
@@ -31,7 +28,7 @@ ${listAdmin}
 ◈ *Autorechazar:* ${autoRechazar ? 'Activado' : 'Desactivado'}
 ◈ *Modohorny:* ${modohorny ? 'Activado' : 'Desactivado'}
 `.trim();
-  conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner, primary]});
+  conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
   } catch (e) {
     return m.reply(`Ocurrió un error inesperado\n\n> ${e}`);
 }};
