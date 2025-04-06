68855fb53737ea592d94d42a644e3960835f4e50
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-04-06 11:17:45 -0400

    Actualizar +18-porno.js

--- plugins/+18-porno.js
+++ plugins/+18-porno.js
@@ -1,12 +1,42 @@
 import axios from 'axios';
-const handler = async (m, {command, conn, usedPrefix}) => {
-if (!db.data.chats[m.chat].nsfw) return m.reply('🚩 *¡Estos comandos están desactivados!*');
-const res = (await axios.get(`https://raw.githubusercontent.com/CheirZ/HuTao-Proyect/master/src/JSON/${command}.json`)).data;
-const haha = await res[Math.floor(res.length * Math.random())];
-conn.sendFile(m.chat, haha, 'error.jpg', `🔥 *${command}*`, m);
+
+const handler = async (m, { command, conn, usedPrefix }) => {
+  let botId = conn.user.jid;
+  let botSettings = global.db.data.settings[botId];
+
+  let botname = botSettings.namebot;
+  let icon = botSettings.icon;
+  let banner = botSettings.banner;
+  let currency = botSettings.currency;
+
+  if (!db.data.chats[m.chat].modohorny && m.isGroup) return m.reply('🚩 *¡Estos comandos están desactivados!*');
+
+  const comandos = ['nsfwloli', 'nsfwfoot', 'nsfwass', 'nsfwbdsm', 'nsfwcum', 'nsfwero', 'nsfwfemdom', 'nsfwfoot', 'nsfwglass', 'nsfworgy', 'yuri', 'yaoi', 'panties', 'tetas', 'booty', 'ecchi', 'furro', 'hentai', 'trapito', 'imagenlesbians', 'pene', 'porno', 'randomxxx', 'pechos'];
+
+  const res = (await axios.get(`https://raw.githubusercontent.com/CheirZ/HuTao-Proyect/master/src/JSON/${command}.json`)).data;
+  const haha = await res[Math.floor(res.length * Math.random())];
+  const caption = `*${command}* 🔥`;
+
+  let otros = comandos.filter(c => c !== command);
+  let [random1, random2] = otros.sort(() => 0.5 - Math.random()).slice(0, 2);
+
+  await conn.sendMessage(m.chat, {
+    image: { url: haha },
+    caption,
+    footer: `${botname}`,
+    buttons: [
+      { buttonId: `.${command}`, buttonText: { displayText: "🔥 sɪɢᴜɪᴇɴᴛᴇ 🔥" }, type: 1 },
+      { buttonId: `.${random1}`, buttonText: { displayText: `🎲 ${random1} 🔥` }, type: 1 },
+      { buttonId: `.${random2}`, buttonText: { displayText: `🎲 ${random2} 🔥` }, type: 1 },
+    ],
+    headerType: 4,
+  }, { quoted: m });
 };
-handler.help = ['nsfwloli', 'nsfwfoot', 'nsfwass', 'nsfwbdsm', 'nsfwcum', 'nsfwero', 'nsfwfemdom', 'nsfwfoot', 'nsfwglass', 'nsfworgy', 'yuri', 'yuri2', 'yaoi', 'yaoi2', 'panties', 'tetas', 'booty', 'ecchi', 'furro', 'hentai', 'trapito', 'imagenlesbians', 'pene', 'porno', 'randomxxx', 'pechos'];
-handler.command = ['nsfwloli', 'nsfwfoot', 'nsfwass', 'nsfwbdsm', 'nsfwcum', 'nsfwero', 'nsfwfemdom', 'nsfwfoot', 'nsfwglass', 'nsfworgy', 'yuri', 'yuri2', 'yaoi', 'yaoi2', 'panties', 'tetas', 'booty', 'ecchi', 'furro', 'hentai', 'trapito', 'imagenlesbians', 'pene', 'porno', 'randomxxx', 'pechos'];
+
+handler.help = ['nsfwloli', 'nsfwfoot', 'nsfwass', 'nsfwbdsm', 'nsfwcum', 'nsfwero', 'nsfwfemdom', 'nsfwfoot', 'nsfwglass', 'nsfworgy', 'yuri', 'yaoi', 'panties', 'tetas', 'booty', 'ecchi', 'furro', 'hentai', 'trapito', 'imagenlesbians', 'pene', 'porno', 'randomxxx', 'pechos'];
+handler.command = handler.help;
 handler.tags = ['nsfw'];
-handler.register = true
+handler.group = true;
+handler.register = true;
+
 export default handler;
