2f7299ac1b24b461e9d8ced22e167be8977e07a5
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-04-26 08:20:07 -0400

    Actualizar rpg-cartera.js

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -3,10 +3,10 @@ let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m
 if (who == conn.user.jid) return error 
 if (!(who in global.db.data.users)) return conn.reply(m.chat, '🍭 El usuario no se encuentra en mi base de Datos.', m, fake)
 let user = global.db.data.users[who]
-await m.reply(`${who == m.sender ? `Tienes *${user.cookies} Cookies 🍪* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.cookies} Cookies 🍪* en su Cartera`}. `, null, { mentions: [who] })}
+await m.reply(`${who == m.sender ? `Tienes *${user.chocolates} Chocolates 🍫* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.chocolates} Chocolates 🍫* en su Cartera`}. `, null, { mentions: [who] })}
 
-handler.help = ['cookies']
+handler.help = ['chocolates']
 handler.tags = ['rpg']
-handler.command = ['wallet', 'cartera', 'cookies', 'bal', 'coins']
+handler.command = ['wallet', 'cartera', 'chocolates', 'bal', 'coins']
 handler.register = true 
 export default handler
