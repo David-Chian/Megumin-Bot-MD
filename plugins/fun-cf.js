0c948ee3869af4bb2910c2ba1c0a82651bdf4a07
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-04-26 08:07:06 -0400

    Actualizar fun-cf.js

--- plugins/fun-cf.js
+++ plugins/fun-cf.js
@@ -3,7 +3,7 @@ let users = {};
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [eleccion, cantidad] = text.split(' ');
     if (!eleccion || !cantidad) {
-        return m.reply(`❤️‍🔥 Por favor, elige cara o cruz y una cantidad de galletas para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
+        return m.reply(`❤️‍🔥 Por favor, elige cara o cruz y una cantidad de Chocolates 🍫 para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
     }
 
     eleccion = eleccion.toLowerCase();
@@ -13,24 +13,24 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     }
 
     if (isNaN(cantidad) || cantidad <= 0) {
-        return m.reply(`❤️‍🔥 Cantidad no válida. Por favor, elige una cantidad de galletas para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
+        return m.reply(`❤️‍🔥 Cantidad no válida. Por favor, elige una cantidad de Chocolates 🍫 para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
     }
 
     let userId = m.sender;
-    if (!users[userId]) users[userId] = { cookies: 100 };
+    if (!users[userId]) users[userId] = { chocolates: 100 };
     let user = global.db.data.users[m.sender];
-    if (user.cookies < cantidad) {
-        return m.reply(`❤️‍🔥 No tienes suficientes galletas para apostar. Tienes ${user.cookies} galletas.`);
+    if (user.chocolates < cantidad) {
+        return m.reply(`❤️‍🔥 No tienes suficientes Chocolates 🍫 para apostar. Tienes ${user.chocolates} Chocolates 🍫.`);
     }
 
     let resultado = Math.random() < 0.5 ? 'cara' : 'cruz';
    let mensaje = `⭐️ La moneda ha caído en `
     if (resultado === eleccion) {
-        user.cookies += cantidad; 
-    mensaje += `*${resultado}* y has ganado *${cantidad} galletas*!`;
+        user.chocolates += cantidad; 
+    mensaje += `*${resultado}* y has ganado *${cantidad} Chocolates 🍫*!`;
     } else {
-        user.cookies -= cantidad;
-        mensaje += `*${resultado}* y has perdido *${cantidad} galletas*!`;
+        user.chocolates -= cantidad;
+        mensaje += `*${resultado}* y has perdido *${cantidad} Chocolates 🍫*!`;
     }
 
     await conn.reply(m.chat, mensaje, m);
