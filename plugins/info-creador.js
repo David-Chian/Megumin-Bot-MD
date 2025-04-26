faa9f7f991972b7bb634769d0abe61738458f882
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-04-26 08:48:46 -0400

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,62 +1,51 @@
-import PhoneNumber from 'awesome-phonenumber'
+let handler = async (m, { conn, command, usedPrefix }) => {
+let creadorID = '5351524614@s.whatsapp.net'
+let isInGroup = m.isGroup && (await conn.groupMetadata(m.chat)).participants.some(p => p.id === creadorID)
 
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-//m.react('⚙️')
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-let biografia = await conn.fetchStatus('5351524614' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}` +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let bio = biografia.status?.toString() || 'Sin Biografía'
-let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
-let name = await conn.getName(who)
+let numeroTexto = isInGroup ? `@${creadorID.split('@')[0]}` : `+53 51524614`
 
-  await sendContactArray(conn, m.chat, [
-     [`${nomorown}`, `👑 Propietario`, `ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆̟(◣_◢)凸`, dev, correo, `🇨🇺 Cuba`, `${global.yt}`, bio],
-[`${conn.user.jid.split('@')[0]}`, `Es Una Bot 🍟`, `${packname}`, `📵 No Hacer Spam`, correo, `🇨🇺 Cuba`, md, biobot]
-], m)
-  //m.reply(`Hola @${m.sender.split(`@`)[0]} este es el contacto de mi creador, no hagas spam!!`)
-  } 
+let creador = `🌹 *C R E A D O R - 💎 - B O T*
 
-handler.help = ["creador","owner"]
-handler.tags = ["info"]
-handler.command = ['owner','creador']
+🌱 *NOMBRE:* ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆̟(◣_◢)凸
+🍟 *NUMERO:* ${numeroTexto}
+🪴 *LINK:* wa.me/5351524614
+
+👑 *E N L A C E S - U T I L E S:*
+
+• *GRUPO OFC:*
+https://chat.whatsapp.com/F4QEFF2Hn4102NdbPJ2ZOi
+• *CANAL OFC*
+https://whatsapp.com/channel/0029VaqAtuIK0IBsHYXtvA3e
+• *GITHUB:*
+https://github.com/David-Chian
+• *YOUTUBE:*
+https://youtube.com/@davidchian4957
+`
+
+await conn.sendMessage(m.chat, {
+  text: creador.trim(),
+  contextInfo: {
+    forwardingScore: 200,
+    isForwarded: false,
+    mentionedJid: isInGroup ? [creadorID] : [],
+    externalAdReply: {
+      showAdAttribution: true,
+      renderLargerThumbnail: true,
+      title: `🥷 Developer 👑`,
+      body: packname,
+      mediaType: 1,
+      sourceUrl: redes,
+      thumbnailUrl: icons
+    }
+  }
+}, {
+  quoted: fkontak
+})
+
+}
+handler.help = ['creador']
+handler.command = ['creador', 'creator', 'owner', 'propietario', 'dueño']
+handler.register = true
+handler.tags = ['main']
+
 export default handler
-
-async function sendContactArray(conn, jid, data, quoted, options) {
-        if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
-                let contacts = []
-        for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
-            number = number.replace(/[^0-9]/g, '')
-            let njid = number + '@s.whatsapp.net'
-            let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
-            // N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
-            let vcard = `
-BEGIN:VCARD
-VERSION:3.0
-N:Sy;Bot;;;
-FN:${name.replace(/\n/g, '\\n')}
-item.ORG:${isi}
-item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
-item1.X-ABLabel:${isi1}
-item2.EMAIL;type=INTERNET:${isi2}
-item2.X-ABLabel:📧 Email
-item3.ADR:;;${isi3};;;;
-item3.X-ABADR:ac
-item3.X-ABLabel:🏷 Region
-item4.URL:${isi4}
-item4.X-ABLabel:Website
-item5.X-ABLabel:${isi5}
-END:VCARD`.trim()
-            contacts.push({ vcard, displayName: name })
-        }
-        return await conn.sendMessage(jid, {
-            contacts: {
-                displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
-                contacts,
-            }
-        },
-        {
-            quoted,
-            ...options
-        })
-        }
