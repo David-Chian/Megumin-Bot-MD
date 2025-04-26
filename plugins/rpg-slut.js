9444c91eadf39574e0be201e1d8b8122b86addd9
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-04-26 08:08:45 -0400

    Actualizar rpg-slut.js

--- plugins/rpg-slut.js
+++ plugins/rpg-slut.js
@@ -12,36 +12,36 @@ m.reply(`🍟 Ya Te Has Postituido Recientemente, Espera ⏱️ *${tiempo2}* Par
 return
 }
 cooldowns[m.sender] = Date.now()
-let senderCoo = users[senderId].cookies || 0
+let senderCoo = users[senderId].chocolates || 0
 let randomUserId = Object.keys(users)[Math.floor(Math.random() * Object.keys(users).length)]
 while (randomUserId === senderId) {
 randomUserId = Object.keys(users)[Math.floor(Math.random() * Object.keys(users).length)]}
-let randomUserCoo = users[randomUserId].cookies || 0
+let randomUserCoo = users[randomUserId].chocolates || 0
 let minAmount = 15
 let maxAmount = 50
 let amountTaken = Math.floor(Math.random() * (maxAmount - minAmount + 1)) + minAmount
 let randomOption = Math.floor(Math.random() * 3)
 switch (randomOption) {
 case 0:
-users[senderId].cookies += amountTaken
-users[randomUserId].cookies -= amountTaken
+users[senderId].chocolates += amountTaken
+users[randomUserId].chocolates -= amountTaken
 conn.sendMessage(m.chat, {
-text: `🚩¡Te Postituiste Y Ganaste *${amountTaken} Cookies 🍪* Dejaste Casi Seco A @${randomUserId.split("@")[0]}\n\nSe suman *+${amountTaken} Cookies 🍪* a ${senderName}.`,
+text: `🚩¡Te Postituiste Y Ganaste *${amountTaken} Chocolates 🍫* Dejaste Casi Seco A @${randomUserId.split("@")[0]}\n\nSe suman *+${amountTaken} Chocolates 🍫* a ${senderName}.`,
 contextInfo: { 
 mentionedJid: [randomUserId],
 }}, { quoted: m })
 break
 case 1:
 let amountSubtracted = Math.min(Math.floor(Math.random() * (senderCoo - minAmount + 1)) + minAmount, maxAmount)
-users[senderId].cookies -= amountSubtracted
-conn.reply(m.chat, `🚩 Le Rompiste La Verga A Tu Cliente Te Cobro Y Se Te Quitan *-${amountSubtracted} Cookies 🍪* a ${senderName}.`, m, rcanal)
+users[senderId].chocolates -= amountSubtracted
+conn.reply(m.chat, `🚩 Le Rompiste La Verga A Tu Cliente Te Cobro Y Se Te Quitan *-${amountSubtracted} Chocolates 🍫* a ${senderName}.`, m, rcanal)
 break
 case 2:
 let smallAmountTaken = Math.min(Math.floor(Math.random() * (randomUserDolares / 2 - minAmount + 1)) + minAmount, maxAmount)
-users[senderId].cookies += smallAmountTaken
-users[randomUserId].cookies -= smallAmountTaken
+users[senderId].chocolates += smallAmountTaken
+users[randomUserId].chocolates -= smallAmountTaken
 conn.sendMessage(m.chat, {
-text: `🚩 Vuelves A Las Calles Y Te Vas A Un Motel Te Paga ${smallAmountTaken} Cookies 🍪* de @${randomUserId.split("@")[0]}\n\nSe suman *+${smallAmountTaken} Cookies 🍪* a ${senderName}.`,
+text: `🚩 Vuelves A Las Calles Y Te Vas A Un Motel Te Paga ${smallAmountTaken} Chocolates 🍫* de @${randomUserId.split("@")[0]}\n\nSe suman *+${smallAmountTaken} Chocolates 🍫* a ${senderName}.`,
 contextInfo: { 
 mentionedJid: [randomUserId],
 }}, { quoted: m })
