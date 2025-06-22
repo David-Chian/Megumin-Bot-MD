78ebf08c1be8e85f21b434b5a26c71b8bd3b9153
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-06-22 13:08:59 -0400

    Actualizar herramientas-miid.js

--- plugins/herramientas-miid.js
+++ plugins/herramientas-miid.js
@@ -4,9 +4,9 @@ const handler = async (m, { conn }) => {
   if (!groupMetadata) return m.reply('❌ No se pudo obtener la información del grupo.')
 
   const participants = groupMetadata.participants || []
-  const user = participants.find(p => p.id.includes(m.sender))
+  /*const user = participants.find(p => p.id.includes(m.sender))
 
-  if (!user) return m.reply('❌ No se pudo encontrar tu ID en el grupo.')
+  if (!user) return m.reply('❌ No se pudo encontrar tu ID en el grupo.')*/
 
   await m.reply(`🆔 Tu ID es:\n${user.id}`)
 }
