6b9331a7652ff103718e579bd202e30d8e5f5395
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-07-30 14:26:44 -0400

    Actualizar fun-sad.js

--- plugins/fun-sad.js
+++ plugins/fun-sad.js
@@ -5,9 +5,9 @@ import path from 'path';
 
 let handler = async (m, { conn, usedPrefix }) => {
     let who;
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? await m.quoted.sender : false;
     else who = m.chat;
-    if (!who) throw 'Etiqueta o menciona a alguien';
+    if (!who) m.reply('Etiqueta o menciona a alguien')
 
     let user = global.db.data.users[who];
     let name = conn.getName(who);
