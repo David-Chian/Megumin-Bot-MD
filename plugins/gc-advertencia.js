a28f1f44939d8621cc05d9e73547df48fc63c256
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-07-30 14:33:30 -0400

    ⚠

--- plugins/gc-advertencia.js
+++ plugins/gc-advertencia.js
@@ -6,7 +6,7 @@ const handler = async (m, {conn, text, command, usedPrefix}) => {
     who = m.mentionedJid[0] ?
       m.mentionedJid[0] :
       m.quoted ?
-      m.quoted.sender :
+      await m.quoted.sender :
       text;
   } else who = m.chat;
   const user = global.db.data.users[who];
