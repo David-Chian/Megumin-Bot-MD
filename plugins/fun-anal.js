adbcd4ac1efb09cabb5dbd3751d421c81e1b6976
ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆(◣_◢)凸 <107944279+David-Chian@users.noreply.github.com>
2025-07-30 14:09:32 -0400

    sexo anal

--- plugins/fun-anal.js
+++ plugins/fun-anal.js
@@ -6,9 +6,9 @@ import { sticker } from '../lib/sticker.js';
 let handler = async (m, { conn, usedPrefix }) => {
     if (!db.data.chats[m.chat].nsfw && m.isGroup) return m.reply('🚩 *¡Estos comandos están desactivados!*');
     let who;
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? await m.quoted.sender : false;
     else who = m.chat;
-    if (!who) throw 'Etiqueta o menciona a alguien para partirle el culo XD';
+    if (!who) m.reply('Etiqueta o menciona a alguien para partirle el culo XD')
 
     let user = global.db.data.users[who];
     let name = conn.getName(who);
